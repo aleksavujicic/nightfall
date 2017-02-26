4eada19aefcf55f6d6c77283e0058830a382689b
Deimophobe <deimophobe@gmail.com>
2017-02-27 01:42:11 +1300

    SOS looks much cooler

--- src/deimophobe/dvz/dwarf/kit/consumable/SOS.java
+++ src/deimophobe/dvz/dwarf/kit/consumable/SOS.java
@@ -1,13 +1,24 @@
 package deimophobe.dvz.dwarf.kit.consumable;
 
+import deimophobe.dvz.Game;
 import deimophobe.dvz.dwarf.Dwarf;
 import deimophobe.dvz.monster.MonsterManager;
 import deimophobe.dvz.monster.MonsterPlayer;
 import deimophobe.dvz.monster.ai.AIEntity;
 import deimophobe.dvz.monster.ai.AIManager;
-import org.bukkit.Location;
-import org.bukkit.Sound;
+import org.bukkit.*;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.MagmaCube;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.EulerAngle;
+import org.bukkit.util.Vector;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Deimophobe on 22/01/17.
@@ -18,8 +29,9 @@ class SOS extends Consumable {
 		super(item);
 	}
 	
-	private static final double RANGE = 30;
+	private static final double RANGE = 12;
 	private static final int FREEZE_TIME = 200;
+	private static final int NUM_SWORDS = 3;
 	
 	@Override
 	public boolean use(Dwarf dwarf) {
@@ -30,6 +42,74 @@ class SOS extends Consumable {
 		}
 		AIManager.getManager().clearArea(center, RANGE);
 		dwarf.playSound("entity.evocation_illager.prepare_summon", 10, 1f, true);
+		
+		
+		// Spawn swords;
+		List<Location> swordLocations = new ArrayList<>();
+		List<ArmorStand> swords = new ArrayList<>();
+		for (int i=0; i<NUM_SWORDS ; i++) {
+			double r = 9 + 2 * Math.random();
+			double theta = (i+0.3 + 0.4*Math.random())*2*Math.PI/NUM_SWORDS; // Ensures the three swords are in three seperate segments.
+			
+			double x = r * Math.cos(theta);
+			double z = r * Math.sin(theta);
+			double y = 2 + 2 * Math.random();
+			
+			Location position = center.clone().add(x, y, z);
+			swords.add(summonSword(position));
+			swordLocations.add(position.add(0, 1, 0));
+		}
+		swordLocations.add(swordLocations.get(0)); // So the list 'wraps around' to first element
+		
+		// Set particle locations
+		Set<Location> particleLocations = new HashSet<>();
+		for (int i=0; i<NUM_SWORDS ; i++) {
+			Location from = swordLocations.get(i).clone();
+			Location to = swordLocations.get(i+1).clone();
+			
+			to.subtract(from);
+			double size = to.length();
+			Vector delta = to.toVector().normalize();
+			for (int j=0; j<size; j++) {
+				particleLocations.add(from.clone());
+				from.add(delta);
+			}
+		}
+		
+		// Spawn particles
+		World world = center.getWorld();
+		BukkitRunnable particleRunner = new BukkitRunnable() {
+			@Override
+			public void run() {
+				for (Location particleLoc : particleLocations) {
+					world.spawnParticle(Particle.END_ROD, particleLoc, 1, 0, 0, 0, 0);
+				}
+			}
+		};
+		particleRunner.runTaskTimer(Game.getGame().getPlugin(), 0, 20);
+		
+		// Remove swords + particles when done
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				particleRunner.cancel();
+				for (ArmorStand sword : swords) {
+					sword.remove();
+				}
+			}
+		}.runTaskLater(Game.getGame().getPlugin(), FREEZE_TIME);
+		
 		return true;
+	}
+	
+	private ArmorStand summonSword(Location loc) {
+		ArmorStand stand = (ArmorStand) loc.getWorld().spawnEntity(loc, EntityType.ARMOR_STAND);
+		stand.setHelmet(getItem());
+		stand.setHeadPose(new EulerAngle(Math.PI, Math.random() * Math.PI * 2, 0));
+		stand.setVisible(false);
+		stand.setBasePlate(false);
+		stand.setGravity(false);
+		
+		return stand;
 	}
 }
