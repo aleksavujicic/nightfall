cdbd75c9d923a8e68f16c7808b985da8193fce95
Deimophobe <deimophobe@gmail.com>
2017-05-16 21:26:23 +1200

    Slightly reworked doom clock item

--- src/main/java/deimophobe/dvz/monster/spawnmenu/DoomClockItem.java
+++ src/main/java/deimophobe/dvz/monster/spawnmenu/DoomClockItem.java
@@ -3,30 +3,49 @@ package deimophobe.dvz.monster.spawnmenu;
 import deimophobe.dvz.Game;
 import deimophobe.dvz.Phase;
 import deimophobe.dvz.menu.MenuItem;
+import deimophobe.dvz.menu.MenuSession;
 import deimophobe.dvz.monster.MonsterPlayer;
 import deimophobe.dvz.monster.doom.DoomManager;
+import org.bukkit.ChatColor;
 import org.bukkit.inventory.ItemStack;
 
 /**
  * Created by Deimophobe on 2/02/17.
  */
-class DoomClockItem extends CostMobMenuItem {
+class DoomClockItem implements MenuItem<MonsterPlayer> {
 	
+	private final ItemStack item;
+	
+	private final int xpCost;
 	private final int time;
 	
 	DoomClockItem(ItemStack item, int xpCost, int time) {
-		super(item, xpCost);
+		this.item = item;
+		
+		this.xpCost = xpCost;
 		this.time = time;
 	}
 	
-	@Override
-	protected boolean onPayCost(MonsterPlayer monster) {
-		DoomManager.getManager().reduceDoom(time);
-		return false;
+	private boolean isAvailable() {
+		return (!DoomManager.getManager().isDoom());
 	}
 	
 	@Override
-	public boolean isAvailable(MonsterPlayer monster) {
-		return (!DoomManager.getManager().isDoom());
+	public ItemStack getDisplayItem(MenuSession<MonsterPlayer> session) {
+		return item;
+	}
+	
+	@Override
+	public final boolean onClick(MenuSession<MonsterPlayer> session) {
+		if (!isAvailable()) return false;
+		
+		MonsterPlayer monster = session.getData();
+		if (monster.useXP(xpCost)) {
+			DoomManager.getManager().reduceDoom(time);
+		} else {
+			monster.sendMessage(ChatColor.RED + "Not enough exp! " + "You have " + ChatColor.AQUA + monster.getXP() +
+					ChatColor.RED + "/" + ChatColor.GREEN + xpCost);
+		}
+		return false;
 	}
 }
