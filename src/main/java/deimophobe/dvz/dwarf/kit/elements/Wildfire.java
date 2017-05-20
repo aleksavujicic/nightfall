6c145477405de3bd707a42db711b1ef31e6a9c0d
Deimophobe <deimophobe@gmail.com>
2017-05-20 13:43:42 +1200

    wildfire tests

--- src/main/java/deimophobe/dvz/dwarf/kit/elements/Wildfire.java
+++ src/main/java/deimophobe/dvz/dwarf/kit/elements/Wildfire.java
@@ -65,7 +65,7 @@ class Wildfire extends AbstractItem {
 	private static final int FLAME_LIFE = 40;
 	private static final int FLAME_DELAY = 3;
 	private static final double FLAME_RADIUS = 2;
-	private static final double FLAME_VELOCITY = 0.8;
+	private static final double FLAME_VELOCITY = 0.4;
 	private static final double FLAME_DPT = 3; // Damage per tick
 	
 	private class Flame {
@@ -75,7 +75,7 @@ class Wildfire extends AbstractItem {
 		
 		private Flame(Location position, Vector velocity) {
 			this.position = position;
-			this.velocity = velocity;
+			this.velocity = dwarf.getVelocity().add(velocity);
 			
 			new BukkitRunnable() {
 				@Override
