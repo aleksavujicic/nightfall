bbab64cb68e1879635df932d1c2c1de250e3b6bf
Deimophobe <deimophobe@gmail.com>
2017-06-15 23:48:52 +1200

    Added test mob

--- src/main/java/deimophobe/dvz/monster/mob/AbstractTypedMob.java
+++ src/main/java/deimophobe/dvz/monster/mob/AbstractTypedMob.java
@@ -51,13 +51,11 @@ abstract class AbstractTypedMob extends AbstractMob {
 		
 		
 		monster.clearEffects();
-		givePermanentPotionEffect(PotionEffectType.NIGHT_VISION, 1);
 		if (mobData.immuneTime != 0) {
 			monster.givePotionEffect(PotionEffectType.LUCK, mobData.immuneTime*20, 1, true, false, true);
 		}
 		
-		monster.teleportTo(ShrineManager.getManager().getCurrentMobspawn());
-		monster.getPlayer().setGameMode(GameMode.SURVIVAL);
+		super.spawn();
 	}
 	
 	
