19f3bd8a42f4712d5a9caafeffcd8511f1cfe246
Deimophobe <deimophobe@gmail.com>
2017-06-22 00:40:34 +1200

    Stonebrick now considered wall

--- src/main/java/deimophobe/dvz/blocks/blocktype/BlockType.java
+++ src/main/java/deimophobe/dvz/blocks/blocktype/BlockType.java
@@ -29,7 +29,7 @@ public class BlockType {
 	
 	public static final CustomBlock CORRODED_WALL = new WoolBlock(DyeColor.LIME);
 	
-	public static final ComparableBlock UNENCHANTED_WALL = new BlockSet(NORMAL_WALL, CRACKED_WALL, DAMAGED_WALL, BROKEN_WALL);
+	public static final ComparableBlock UNENCHANTED_WALL = new BlockSet(NORMAL_WALL, CRACKED_WALL, DAMAGED_WALL, BROKEN_WALL, new MaterialBlock(Material.SMOOTH_BRICK));
 	public static final ComparableBlock WALL = new BlockSet(ENCHANTED_WALL, UNENCHANTED_WALL);
 	
 	
