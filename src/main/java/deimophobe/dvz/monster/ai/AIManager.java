3477e92f945f494d5f15c9b58f82640df9a9a0e2
Deimophobe <deimophobe@gmail.com>
2017-06-27 15:14:39 +1200

    Rebalanced ai quantities

--- src/main/java/deimophobe/dvz/monster/ai/AIManager.java
+++ src/main/java/deimophobe/dvz/monster/ai/AIManager.java
@@ -130,7 +130,7 @@ public class AIManager {
 			int dwarves = DwarfManager.getManager().getNumberOfPlayers();
 			int mobs = MonsterManager.getManager().getNumberOfPlayers();
 			
-			double spawnChance = (10 + mobs + dwarves*2) * 0.008;
+			double spawnChance = (10 + mobs + dwarves*4) * 0.004;
 			spawnChance += (Game.getGame().isNight() ? 0.05 : 0);
 			
 			int maxAIs = BASE_MAX_AIS;
