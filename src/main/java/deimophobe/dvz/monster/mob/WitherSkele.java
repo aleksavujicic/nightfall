70cdaccd949c973a4ed7117d5df11f6d3aa274d1
Deimophobe <deimophobe@gmail.com>
2017-06-29 01:20:56 +1200

    Improved wither skele and reduced special spawns

--- src/main/java/deimophobe/dvz/monster/mob/WitherSkele.java
+++ src/main/java/deimophobe/dvz/monster/mob/WitherSkele.java
@@ -1,5 +1,7 @@
 package deimophobe.dvz.monster.mob;
 
+import deimophobe.dvz.damage.DamageType;
+import deimophobe.dvz.dwarf.Dwarf;
 import deimophobe.dvz.monster.MonsterPlayer;
 
 /**
@@ -9,6 +11,7 @@ class WitherSkele extends SkeletonMob {
 	
 	@Override protected MobType getType() {return MobType.WITHERSKELE;}
 	@Override protected double getPower() {return 40;}
+	private double damageBoost = 0;
 	
 	WitherSkele(MonsterPlayer monster) {
 		super(monster);
@@ -18,5 +21,23 @@ class WitherSkele extends SkeletonMob {
 	public void spawn() {
 		super.spawn();
 		giveArrows(128);
+	}
+	
+	@Override
+	public void update(boolean quartSec, boolean halfSec, boolean sec, boolean doubleSec, boolean quadSec) {
+		if (sec && damageBoost > 0)
+			damageBoost = Math.max(damageBoost - 1, 0);
+	}
+	
+	@Override
+	public double onHit(Dwarf dwarf, DamageType type, double damage) {
+		if (type.isArrow()) {
+			damageBoost += 5;
+			dwarf.getArmour().damage((int) damageBoost*3 + 20);
+			monster.heal(5);
+			return getPower() + damageBoost;
+		} else {
+			return damage;
+		}
 	}
 }
