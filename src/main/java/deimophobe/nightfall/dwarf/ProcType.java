fc6c8cfed1703e5d9017fea5486f3c28550d00f0
Deimophobe <deimophobe@gmail.com>
2017-07-06 13:27:45 +1200

    Fixed dash proc getting overwritten

--- src/main/java/deimophobe/nightfall/dwarf/ProcType.java
+++ src/main/java/deimophobe/nightfall/dwarf/ProcType.java
@@ -7,19 +7,19 @@ import org.bukkit.potion.PotionEffectType;
  * Created by Deimophobe on 6/05/17.
  */
 public enum ProcType {
-	REGULAR		(60, 1, 2, 0, true, true),
-	DRAGONSKIN	(60, 1, 1, 0, true, true),
-	GRAVEL_PROC	(60, 1, 4, 0, true, true),
-	EBOW		(60, 1, 3, 0, true, true),
+	REGULAR		(60, 1, 2, 0, true, true, true),
+	DRAGONSKIN	(60, 1, 1, 0, true, true, true),
+	GRAVEL_PROC	(60, 1, 4, 0, true, true, true),
+	EBOW		(60, 1, 3, 0, true, true, true),
 	
-	HORN		(160, 4, 2, 3, true, true),
-	AVENGE		(160, 4, 2, 3, true, true),
-	MALICE		(160, 2, 4, 0, true, true),
-	SHRINE_FALL	(160, 2, 2, 3, false, false),
+	HORN		(160, 4, 2, 3, true, true, true),
+	AVENGE		(160, 4, 2, 3, true, true, true),
+	MALICE		(160, 2, 4, 0, true, true, true),
+	SHRINE_FALL	(160, 2, 2, 3, false, false, true),
 	
-	RUNEDASH	(12, 1, 10, 0, false, false),
+	RUNEDASH	(12, 1, 10, 0, false, false, false),
 	
-	ROAR		(160, 2, 0, 0, false, false),
+	ROAR		(160, 2, 0, 0, false, false, true),
 	
 	;
 	
@@ -31,20 +31,23 @@ public enum ProcType {
 	private final boolean showParticles;
 	private final boolean playSound;
 	
-	ProcType(int duration, int speedLvl, int strLvl, int hasteLvl, boolean showParticles, boolean playSound) {
+	private final boolean overwrite;
+	
+	ProcType(int duration, int speedLvl, int strLvl, int hasteLvl, boolean showParticles, boolean playSound, boolean overwrite) {
 		this.duration = duration;
 		this.speedLvl = speedLvl;
 		this.strLvl = strLvl;
 		this.hasteLvl = hasteLvl;
 		this.showParticles = showParticles;
 		this.playSound = playSound;
+		this.overwrite = overwrite;
 	}
 	
 	
 	void giveProc(Dwarf dwarf) {
-		dwarf.givePotionEffect(PotionEffectType.SPEED, duration, speedLvl, true, false, true);
-		dwarf.givePotionEffect(PotionEffectType.INCREASE_DAMAGE, duration, strLvl, true, false, false);
-		dwarf.givePotionEffect(PotionEffectType.FAST_DIGGING, duration, hasteLvl, true, false, false);
+		dwarf.givePotionEffect(PotionEffectType.SPEED, duration, speedLvl, true, false, overwrite);
+		dwarf.givePotionEffect(PotionEffectType.INCREASE_DAMAGE, duration, strLvl, true, false, overwrite);
+		dwarf.givePotionEffect(PotionEffectType.FAST_DIGGING, duration, hasteLvl, true, false, overwrite);
 		
 		if (showParticles)
 			dwarf.getPlayer().getWorld().spawnParticle(Particle.VILLAGER_HAPPY, dwarf.getPlayer().getLocation(), 60, 1, 1, 1);
