0f985a9bc4bf2c72ca7fab3655542f29a0a482d0
Deimophobe <deimophobe@gmail.com>
2017-07-10 18:46:37 +1200

    Nerfed evis

--- src/main/java/deimophobe/nightfall/dwarf/kit/elements/Dagger.java
+++ src/main/java/deimophobe/nightfall/dwarf/kit/elements/Dagger.java
@@ -53,7 +53,7 @@ class Dagger extends AbstractCooldownItem {
 			if (closestMonster != null) {
 				Location loc = closestMonster.getPlayer().getEyeLocation();
 				
-				closestMonster.customDamage(dwarf, DamageType.EVISCERATE, 140);
+				closestMonster.customDamage(dwarf, DamageType.EVISCERATE, 100);
 				loc.getWorld().spawnParticle(Particle.REDSTONE, loc, 20, 0.3, 0.3, 0.3, 1);
 				//world.spigot().playEffect(loc, GameEffect.COLOURED_DUST, 0, 1, red, green, blue, 1, 0, 64);
 				//world.spawnParticle(Particle.SPELL_INSTANT, ltarget.getEyeLocation(), 1, 0.3, 0.3, 0.3, 0);
