96c3a9e96326ac6383d01f6b69449ffac3847a89
Deimophobe <deimophobe@gmail.com>
2017-09-04 20:44:30 +1200

    Made dragonskin a toggle bow (still needs work)

--- src/main/java/deimophobe/nightfall/dwarf/kit/elements/Dragonskin.java
+++ src/main/java/deimophobe/nightfall/dwarf/kit/elements/Dragonskin.java
@@ -6,17 +6,18 @@ import deimophobe.nightfall.dwarf.DwarvenItems;
 import deimophobe.nightfall.dwarf.ProcType;
 import deimophobe.nightfall.dwarf.kit.KitGiveType;
 import deimophobe.nightfall.items.CustomItem;
+import org.bukkit.entity.Projectile;
 
 /**
  * Created by Deimophobe on 20/01/17.
  */
-class Dragonskin extends AbstractBow {
+class Dragonskin extends AbstractToggleBow {
 	
 	Dragonskin(Dwarf dwarf) {
 		super(dwarf);
 	}
 	
-	private final static int POWER = 40;
+	private final static int POWER = 50;
 	private final static CustomItem ITEM = DwarvenItems.getBow("dragonskin", POWER);
 	@Override public CustomItem getItem() {
 		return ITEM;
@@ -26,8 +27,24 @@ class Dragonskin extends AbstractBow {
 	@Override public int getPower() {return POWER;}
 	
 	@Override
+	public void onDamageAttack(MonsterDamage damage) {
+		if (damageFromItem(damage) && damage.hasArrowData()) {
+			Projectile arrow  = damage.arrowData().getArrow();
+			if (isActiveProjectile(arrow)) {
+				damage.setDamage(100);
+				damage.multiplyArrowRes(0.5);
+			}
+		}
+	}
+	
+	@Override
 	public void onKill(MonsterDamage damage) {
 		if (damageFromItem(damage))
 			dwarf.giveProc(ProcType.DRAGONSKIN);
+	}
+	
+	@Override
+	protected boolean canToggle() {
+		return true;
 	}
 }
