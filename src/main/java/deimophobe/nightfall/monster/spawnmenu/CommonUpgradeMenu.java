1c184b7839975d3324f2e1e3fcbf080451141aa2
jameskang105 <jameskang105@gmail.com>
2017-09-21 01:36:29 -0700

    Renamed UpgradeMenu to CommonUpgradeMenu

---
+++ src/main/java/deimophobe/nightfall/monster/spawnmenu/CommonUpgradeMenu.java
@@ -0,0 +1,43 @@
+package deimophobe.nightfall.monster.spawnmenu;
+
+import deimophobe.nightfall.menu.MenuItem;
+import deimophobe.nightfall.menu.MultiItem;
+import deimophobe.nightfall.menu.SimpleMenu;
+import deimophobe.nightfall.monster.MonsterPlayer;
+import deimophobe.nightfall.monster.mob.MobType;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.Set;
+
+/**
+ * Created by Deimophobe on 2/02/17.
+ */
+class CommonUpgradeMenu extends SimpleMenu<MonsterPlayer> {
+    private static final int MENU_SIZE = 27;
+    private final Set<String> upgrades;
+
+    CommonUpgradeMenu(ConfigurationSection section, MobType type) {
+        super(MENU_SIZE);
+
+        upgrades = section.getKeys(false);
+
+        for (String key : section.getKeys(false)) {
+            ConfigurationSection itemSection = section.getConfigurationSection(key);
+            MenuItem<MonsterPlayer> item = new UpgradeMenuItem(itemSection, type);
+            int index = itemSection.getInt("index");
+            insertItem(index, item);
+        }
+    }
+
+    private void insertItem(int index, MenuItem<MonsterPlayer> item) {
+        MultiItem<MonsterPlayer> multiItem = (MultiItem<MonsterPlayer>) getItem(index);
+        if (multiItem == null) multiItem = new MultiItem<>();
+
+        multiItem.addItem(item);
+        setItem(index, multiItem);
+    }
+
+    Set<String> getUpgrades() {
+        return upgrades;
+    }
+}
