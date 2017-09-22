70848d1eab68f61ea551b751ecd4481ed79bc0f6
jameskang105 <jameskang105@gmail.com>
2017-09-22 12:50:17 -0700

    ZombieCommonUpgradeMenu reverted to make it subclass agnostic, probably better OOP design that way

--- src/main/java/deimophobe/nightfall/monster/spawnmenu/UpgradeMenu.java
+++ src/main/java/deimophobe/nightfall/monster/spawnmenu/UpgradeMenu.java
@@ -13,31 +13,31 @@ import java.util.Set;
  * Created by Deimophobe on 2/02/17.
  */
 class UpgradeMenu extends SimpleMenu<MonsterPlayer> {
-	private static final int MENU_SIZE = 27;
-	private final Set<String> upgrades;
-	
-	UpgradeMenu(ConfigurationSection section, MobType type) {
-		super(MENU_SIZE);
-		
-		upgrades = section.getKeys(false);
-		
-		for (String key : section.getKeys(false)) {
-			ConfigurationSection itemSection = section.getConfigurationSection(key);
-			MenuItem<MonsterPlayer> item = new UpgradeMenuItem(itemSection, type);
-			int index = itemSection.getInt("index");
-			insertItem(index, item);
-		}
-	}
-	
-	private void insertItem(int index, MenuItem<MonsterPlayer> item) {
-		MultiItem<MonsterPlayer> multiItem = (MultiItem<MonsterPlayer>) getItem(index);
-		if (multiItem == null) multiItem = new MultiItem<>();
-		
-		multiItem.addItem(item);
-		setItem(index, multiItem);
-	}
-	
-	Set<String> getUpgrades() {
-		return upgrades;
-	}
+    private static final int MENU_SIZE = 27;
+    private final Set<String> upgrades;
+
+    UpgradeMenu(ConfigurationSection section, MobType type) {
+        super(MENU_SIZE);
+
+        upgrades = section.getKeys(false);
+
+        for (String key : section.getKeys(false)) {
+            ConfigurationSection itemSection = section.getConfigurationSection(key);
+            MenuItem<MonsterPlayer> item = new UpgradeMenuItem(itemSection, type);
+            int index = itemSection.getInt("index");
+            insertItem(index, item);
+        }
+    }
+
+    private void insertItem(int index, MenuItem<MonsterPlayer> item) {
+        MultiItem<MonsterPlayer> multiItem = (MultiItem<MonsterPlayer>) getItem(index);
+        if (multiItem == null) multiItem = new MultiItem<>();
+
+        multiItem.addItem(item);
+        setItem(index, multiItem);
+    }
+
+    Set<String> getUpgrades() {
+        return upgrades;
+    }
 }
