1a41a422321549eac2dd66828605a511b16ba6f9
Deimophobe <deimophobe@gmail.com>
2017-10-06 12:50:29 +1300

    Flamelancers can light grass on fire

--- src/main/java/deimophobe/nightfall/blocks/blocktype/BlockType.java
+++ src/main/java/deimophobe/nightfall/blocks/blocktype/BlockType.java
@@ -163,6 +163,9 @@ public class BlockType {
 					Material.AIR
 			)
 	);
+	public static final ComparableBlock IGNITEABLE = new MaterialSet(
+			Material.AIR, Material.DOUBLE_PLANT, Material.LONG_GRASS, Material.YELLOW_FLOWER, Material.RED_ROSE
+	);
 	
 	public static boolean tryConvertBlock(Block block, ComparableBlock from, SettableBlock to) {
 		boolean matches = from.matchesBlock(block);
