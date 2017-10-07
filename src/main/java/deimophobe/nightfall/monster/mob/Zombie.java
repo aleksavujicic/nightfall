83ac2212610cbc418f635ff8a7d0735b47739faa
jameskang105 <jameskang105@gmail.com>
2017-10-06 19:27:08 -0700

    zombie stuff commented out ported to fury

--- src/main/java/deimophobe/nightfall/monster/mob/Zombie.java
+++ src/main/java/deimophobe/nightfall/monster/mob/Zombie.java
@@ -24,21 +24,6 @@ import java.util.Map;
  * Created by Deimophobe on 2/02/17.
  */
 public class Zombie extends AbstractMob {
-	/*
-	private final Cooldown leapCD;
-	private final int leapLvl;
-	
-	private final int pursuit;
-	private final int vampirism;
-	
-	private final double arrowRes;
-	private final int armourShred;
-	
-	private final double rebirthChance;
-	
-	private final boolean fury;
-	private final ComplexCooldown furySound;
-	*/
 
 	protected double rebirthChance;
 	protected Map<String, Integer> upgrades;
@@ -58,59 +43,12 @@ public class Zombie extends AbstractMob {
 		this.rebirthChance = 0;
 		this.rebirthLoc = rebirth;
 		
-		// TODO: Make these not hardcoded. Requires bit of work so later.
-		/*
-		Integer[] shredValues = {0, 5, 8, 12, 15, 20};
-		Integer[] arrowResValues = {0, 25, 40, 50};
-		Integer[] rebirthValues = {0, 25, 50, 60, 70, 75};
-		*/
-		
-		
 		upgrades = monster.getUpgrades(MobType.ZOMBIE);
 		
 		int attack = upgrades.get("attack") + upgrades.get("attack-inf");
 		int health = (upgrades.get("health") + upgrades.get("health-inf"))*2;
 		getWeapon().addModifier(ItemModifierType.ATTACK, attack, "Upgrade");
 		getArmour().addModifier(ItemModifierType.HEALTH, health, "Upgrade");
-
-		/*
-		int leapLvl = upgrades.get("leap");
-		if (leapLvl != 0)
-			leapCD = new SimpleCooldown(200);
-		else
-			leapCD = new DudCooldown();
-		this.leapLvl = upgrades.get("leap");
-
-
-		this.pursuit = upgrades.get("pursuit");
-		
-
-		this.armourShred = shredValues[upgrades.computeIfAbsent("shred", (k) -> 0)];
-		this.vampirism = upgrades.computeIfAbsent("vampirism", (k) -> 0);
-
-		int arrowRes = arrowResValues[upgrades.get("arrow")];
-		this.arrowRes = (double) arrowRes/100;
-
-		getArmour().addModifier(ItemModifierType.ARROW_RESISTANCE, arrowRes, "Upgrade");
-		getWeapon().addModifier(ItemModifierType.ARMOUR_SHRED, armourShred, "Upgrade");
-		
-		int rebirthChance = rebirthValues[upgrades.get("rebirth")];
-		this.rebirthChance = (double) rebirthChance/100;
-		
-		this.fury = upgrades.get("fury") >= 1;
-		
-		if (fury)
-			furySound = new ComplexCooldown(10, () -> {
-				if (Game.getGame().isNight()) monster.playSound("entity.zombie_villager.converted", 1f, 1.5f, true);
-			}, ComplexCooldown.DO_NOTHING);
-		else
-			furySound = new ComplexCooldown(10);
-		
-
-		*/
-
-
-
 	}
 	
 	private boolean didRebirth() {
@@ -140,54 +78,5 @@ public class Zombie extends AbstractMob {
 		else
 			monster.removeRebirth();
 	}
-	/*
-	@Override
-
-	public void update(boolean a, boolean b, boolean c, boolean d, boolean e) {
-		leapCD.update();
-	}
-
-	@Override
-	public void onDamageReceive(MonsterDamage<? extends Dwarf> damage) {
-		super.onDamageReceive(damage);
-		damage.addArrowRes(arrowRes);
-	}
-	
-	@Override
-	public void onUse(Action action, Block block, BlockFace face) {
-		if (Misc.isRightClick(action) && isPlayerHoldingWeapon()) {
-			if (leapCD.isAvailable()) {
-				leapCD.reset();
-				
-				double yaw = monster.getPlayer().getLocation().getYaw();
-				double radYaw = yaw*Math.PI/180;
-				
-				double hVel = (double) leapLvl/2;
-				double vVel = (double) leapLvl/10;
-				monster.getPlayer().setVelocity(new Vector(-hVel * Math.sin(radYaw), vVel, hVel * Math.cos(radYaw)));
-			}
-		}
-	}
-	
-	@Override
-	public void onDamageAttack(DwarfDamage damage) {
-		super.onDamageAttack(damage);
-		
-		damage.addArmourShred(armourShred);
-		
-		int healAmt = vampirism;
-		if (fury) {
-			healAmt += 5;
-			furySound.tryUse();
-		}
-		monster.heal(healAmt);
-		monster.givePotionEffect(PotionEffectType.SPEED, 140, pursuit, true, false, true);
-	}
-	
-	@Override
-	public float getCooldown() {
-		return leapCD.fractionComplete();
-	}
-	*/
 
 }
