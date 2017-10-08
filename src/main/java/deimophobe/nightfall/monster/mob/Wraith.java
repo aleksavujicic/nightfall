22c28ee15dfce9b4333eea02e6f9711913882bc4
jameskang105 <jameskang105@gmail.com>
2017-10-08 15:17:10 -0700

    Wraiths should actually hurt a bit more now

--- src/main/java/deimophobe/nightfall/monster/mob/Wraith.java
+++ src/main/java/deimophobe/nightfall/monster/mob/Wraith.java
@@ -133,8 +133,8 @@ class Wraith extends AbstractMob {
 	}
 	
 	private static final double AOE_RADIUS = 3.5;
-	private static final int AOE_DMG = 50; // This is a one off hit so its not as strong as it seems.
-	private static final int AOE_SHRED = 50;
+	private static final int AOE_DMG = 80; // This is a one off hit so its not as strong as it seems.
+	private static final int AOE_SHRED = 100;
 	private void aoeDamage() {
 		//DamageManager.getManager().AOEDamage(DwarfManager.getManager().getDwarves(), monster,
 		//		CustomDamageType.WRAITH_CHARGE, AOE_RADIUS, AOE_DMG, 1,
@@ -144,6 +144,9 @@ class Wraith extends AbstractMob {
 			if (monster.distanceTo(dwarf) <= AOE_RADIUS) {
 				DwarfDamage damage = dwarf.createDamage(monster, CustomDamageType.WRAITH_CHARGE, AOE_DMG);
 				damage.setArmourShred(AOE_SHRED);
+				damage.getDwarf().givePotionEffect(PotionEffectType.BLINDNESS, 15, 1, false, true, true);
+				damage.getDwarf().givePotionEffect(PotionEffectType.SLOW, 30, 3, false, true, true);
+				damage.getDwarf().givePotionEffect(PotionEffectType.WITHER, 40, 5, false, true, true);
 				damage.setManaDrain(50);
 				damage.setNoDmgTicks(10);
 				damage.fire();
