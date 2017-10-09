07ee8ae9c4d09c7ed2045039d02f0e816c047f27
jameskang105 <jameskang105@gmail.com>
2017-10-08 23:41:00 -0700

    Saboteur base speed nerfed from 50% to 20%

--- src/main/java/deimophobe/nightfall/monster/mob/Zombie_Saboteur.java
+++ src/main/java/deimophobe/nightfall/monster/mob/Zombie_Saboteur.java
@@ -86,7 +86,7 @@ public class Zombie_Saboteur extends Zombie {
         getWeapon().addModifier(ItemModifierType.ARMOUR_SHRED, armourShred, "Upgrade");
         getWeapon().addModifier(ItemModifierType.ATTACK, 5, "Saboteur Zombie");
         getArmour().addModifier(ItemModifierType.HEALTH, -5, "Saboteur Zombie");
-        getArmour().addModifier(ItemModifierType.SPEED, 50, "Saboteur Zombie");
+        getArmour().addModifier(ItemModifierType.SPEED, 20, "Saboteur Zombie");
         getArmour().addModifier(ItemModifierType.SPEED, speed, "Epinephrine");
     }
 
