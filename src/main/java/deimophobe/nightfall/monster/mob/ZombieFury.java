5e701f091982c05b8dcbb63ce242b9901e908b04
jameskang105 <jameskang105@gmail.com>
2017-10-22 18:51:41 -0700

    Bit more arrow res on fury zombies

--- src/main/java/deimophobe/nightfall/monster/mob/ZombieFury.java
+++ src/main/java/deimophobe/nightfall/monster/mob/ZombieFury.java
@@ -34,7 +34,7 @@ public class ZombieFury extends Zombie {
     private final ComplexCooldown furySound;
 
     private static Integer[] shredValues = {0, 6, 12, 18, 24, 30};
-    private static Integer[] arrowResValues = {0, 25, 40, 50};
+    private static Integer[] arrowResValues = {0, 20, 40, 60};
     private static Integer[] rebirthValues = {0, 36, 72, 108, 144, 180};
 
     protected ZombieFury(MonsterPlayer mons) {
