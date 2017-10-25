c2c6bcd5fd4ce08b811327114ed5afa97541be4f
jameskang105 <jameskang105@gmail.com>
2017-10-25 01:54:33 -0700

    less fire for blaze

--- src/main/java/deimophobe/nightfall/monster/mob/Blaze.java
+++ src/main/java/deimophobe/nightfall/monster/mob/Blaze.java
@@ -203,7 +203,7 @@ public class Blaze extends AbstractMob {
                     Block block = centerLoc.clone().add(x, y, z).getBlock();
                     Block blockBelow = centerLoc.clone().add(x,y-1, z).getBlock();
 
-                    if (block.getType() == Material.AIR && blockBelow.getType() != Material.AIR && (Math.random() < 0.008 * flame)) {
+                    if (block.getType() == Material.AIR && blockBelow.getType() != Material.AIR && (Math.random() < 0.003 * flame)) {
                         block.setType(Material.FIRE);
                     }
                 }
