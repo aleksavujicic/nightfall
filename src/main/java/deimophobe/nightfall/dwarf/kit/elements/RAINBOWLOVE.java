e886108b38faf0cc5b2a8afb3128a84f425d8a31
BuildTools <unconfigured@null.spigotmc.org>
2017-10-26 05:28:19 -0400

    I made scepter of magma, healing glow, and rainbow love.  Rainbow love's aoe cooldown reduction is still in development, I've kinda hit a wall there.  Healing glow is basically more expensive jimmy juice right now until we can get the mana and health regen upon taking fire damage working.  Scepter of magma needs balancing.

---
+++ src/main/java/deimophobe/nightfall/dwarf/kit/elements/RAINBOWLOVE.java
@@ -0,0 +1,86 @@
+package deimophobe.nightfall.dwarf.kit.elements;
+
+
+import deimophobe.nightfall.Misc;
+import deimophobe.nightfall.cooldown.ComplexCooldown;
+import deimophobe.nightfall.damage.GameDamage;
+import deimophobe.nightfall.damage.MonsterDamage;
+import deimophobe.nightfall.damage.type.CustomDamageType;
+import deimophobe.nightfall.dwarf.Dwarf;
+import deimophobe.nightfall.dwarf.DwarfManager;
+import deimophobe.nightfall.dwarf.ProcType;
+import deimophobe.nightfall.dwarf.kit.KitCooldownElement;
+import deimophobe.nightfall.dwarf.kit.KitGiveType;
+import deimophobe.nightfall.entity.GameEntity;
+import deimophobe.nightfall.items.CustomItem;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.block.Action;
+import org.bukkit.inventory.ItemStack;
+
+class RAINBOWLOVE extends AbstractElement implements KitCooldownElement{
+
+    private final ComplexCooldown cd = new ComplexCooldown(90*20);
+
+
+    public RAINBOWLOVE(Dwarf dwarf) {
+        super(dwarf);
+    }
+
+    @Override
+    public float fractionComplete() {
+        return 0;
+    }
+
+    @Override
+    public void onDamageAttack (MonsterDamage damage) {
+        dwarf.useMana(5);
+        dwarf.heal(1);
+    }
+
+    @Override
+    public void onKill (MonsterDamage damage) {
+        dwarf.regenMana( 50);
+        cd.reduceCooldown(3*20);
+        if (dwarf.hasProc()) {
+            dwarf.giveProc(ProcType.RAINBOWLOVE);
+        }
+    }
+
+    public boolean onUse(Action action, Block clickedBlock, BlockFace blockFace){
+        if (Misc.isRightClick(action)){
+            cd.tryUse();
+
+            Location center = dwarf.getLocation();
+            double radius = getRadius();
+            for (GameEntity entity: DwarfManager.getManager().getDwarves())
+                if (entity == dwarf)
+                    if (center.distance(entity.getLocation()) <= radius){
+                }
+        }
+        return false;
+    }
+
+    protected double getRadius(){return 13;}
+
+    @Override
+    public void onShift (boolean shift) {
+        if (dwarf.tryUseMana(200)){
+            dwarf.healMax();
+            dwarf.getArmour().repair(1000);
+        }
+    }
+
+    @Override
+    public ItemStack getCooldownToggleItem() {
+        return null;
+    }
+
+}
+//    @Override
+//    public void onDamageReceive (CustomDamageType RAINBOWLOVE){
+//        if (dwarf.hasKitCooldownElement){
+//            cd.reduceCooldown(2*60*20);
+//        }
+//    }
