2042ca2c0e06c1fc220f43bff51ca2279ed86d13
jameskang105 <jameskang105@gmail.com>
2017-10-26 18:46:22 -0700

    Made many more steps to zombie upgrades so that they feel less tedious to get

--- src/main/java/deimophobe/nightfall/monster/mob/ZombieFury.java
+++ src/main/java/deimophobe/nightfall/monster/mob/ZombieFury.java
@@ -33,9 +33,9 @@ public class ZombieFury extends Zombie {
     private final boolean fury;
     private final ComplexCooldown furySound;
 
-    private static Integer[] shredValues = {0, 6, 12, 18, 24, 30};
+    private static Integer[] shredValues = {0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30};
     private static Integer[] arrowResValues = {0, 10, 20, 30, 40, 50};
-    private static Integer[] rebirthValues = {0, 20, 40, 60, 80, 100};
+    private static Integer[] rebirthValues = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 
     protected ZombieFury(MonsterPlayer mons) {
         this(mons, null);
