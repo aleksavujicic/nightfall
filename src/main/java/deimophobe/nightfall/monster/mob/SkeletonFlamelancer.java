a1463bd2eea244c64f457c2e43761c188a583703
jameskang105 <jameskang105@gmail.com>
2017-11-05 03:45:16 -0800

    everything but fire ai should be done

--- src/main/java/deimophobe/nightfall/monster/mob/SkeletonFlamelancer.java
+++ src/main/java/deimophobe/nightfall/monster/mob/SkeletonFlamelancer.java
@@ -5,6 +5,7 @@ import deimophobe.nightfall.Misc;
 import deimophobe.nightfall.blocks.blocktype.BlockType;
 import deimophobe.nightfall.damage.DwarfDamage;
 import deimophobe.nightfall.damage.type.NaturalDamageType;
+import deimophobe.nightfall.items.modifiers.ItemModifierType;
 import deimophobe.nightfall.monster.MonsterPlayer;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -17,14 +18,32 @@ import org.bukkit.entity.Projectile;
  */
 class SkeletonFlamelancer extends Skeleton {
 
+	private int flame;
+	private int volley;
+	private int speed;
+	private int arrowRes;
+	private int firePath;
+	private int fireAI;
+
+	private static final int ARROWS_FIRED = 25;
+	private double flameBlock;
+
 	SkeletonFlamelancer(MonsterPlayer monster) {
 		super(monster, MobData.getMobData("skeleton.flamelancer"));
+		this.flame = upgrades.get("flame");
+		this.volley = upgrades.get("volley");
+		this.speed = upgrades.get("speed");
+		this.arrowRes = upgrades.get("arrowres");
+		this.firePath = upgrades.get("firepath");
+		this.fireAI = upgrades.get("fireai");
+		realArrowRes = arrowRes * 0.01;
+
+		flameBlock = 0.15 + flame * 0.5;
+
+		getArmour().addModifier(ItemModifierType.SPEED, 10, "Flamelancer");
+		getArmour().addModifier(ItemModifierType.SPEED, speed * 10, "Upgrade");
+		getArmour().addModifier(ItemModifierType.ARROW_RESISTANCE, arrowRes * 10, "Upgrade");
 	}
-	
-	private static final int ARROWS_FIRED = 15;
-	
-	private static final double FLAME_CHANCE_STAND = 0.15;
-	private static final double FLAME_CHANCE_ARROW = 0.3;
 	
 	@Override
 	public void onSpawn() {
@@ -32,7 +51,14 @@ class SkeletonFlamelancer extends Skeleton {
 		getDisguise().getWatcher().setBurning(true);
 		giveArrows(64);
 	}
-	
+
+	@Override
+	public void update(boolean quartSec, boolean halfSec, boolean sec, boolean doubleSec, boolean quadSec) {
+        Block block = monster.getLocation().getBlock();
+		if (BlockType.IGNORABLE.matchesBlock(block) && Math.random() < firePath * 0.15) {
+			block.setType(Material.FIRE);
+		}
+	}
 	
 	@Override
 	public Projectile onBowFire(Arrow arrow, float force) {
@@ -41,17 +67,13 @@ class SkeletonFlamelancer extends Skeleton {
 		arrow.setFireTicks(10000);
 		arrow.setCritical(false);
 		final int arrowsToFire = (int) (ARROWS_FIRED*(force*force));
-		for (int i=0; i<arrowsToFire; i++) {
-			Arrow newArrow = ArrowMisc.summonArrow(monster, power, force*2, force, 30f);
-			newArrow.setCritical(false);
-			newArrow.setFireTicks(10000);
+		if (Math.random() < (volley * 0.05 + 0.25)) {
+			for (int i=0; i<arrowsToFire; i++) {
+				Arrow newArrow = ArrowMisc.summonArrow(monster, getPower(), force*2, force, 30f);
+				newArrow.setCritical(false);
+				newArrow.setFireTicks(10000);
+			}
 		}
-		
-		Block block = monster.getPlayer().getLocation().getBlock();
-		if (block.getType() == Material.AIR && Math.random() < FLAME_CHANCE_STAND) {
-			block.setType(Material.FIRE);
-		}
-		
 		return arrow;
 	}
 	
@@ -60,8 +82,12 @@ class SkeletonFlamelancer extends Skeleton {
 		BlockFace face = Misc.getBlockFaceProjectileHit(proj, hitBlock);
 		Block block = hitBlock.getRelative(face);
 		
-		if (BlockType.IGNITEABLE.matchesBlock(block) && Math.random() < FLAME_CHANCE_ARROW) {
-			block.setType(Material.FIRE);
+		if (Math.random() < flameBlock) {
+			if (BlockType.IGNORABLE.matchesBlock(hitBlock)) {
+				hitBlock.setType(Material.FIRE);
+			} else if (BlockType.IGNORABLE.matchesBlock(block)) {
+				block.setType(Material.FIRE);
+			}
 		}
 	}
 	
@@ -70,10 +96,6 @@ class SkeletonFlamelancer extends Skeleton {
 	@Override
 	public void onDamageAttack(DwarfDamage damage) {
 		super.onDamageAttack(damage);
-		if (damage.getType() == NaturalDamageType.RANGED) {
-			damage.setArmourShred(30);
-		}
+		damage.getDwarf().getPlayer().setFireTicks(30 + flame*10);
 	}
-	
-	
 }
