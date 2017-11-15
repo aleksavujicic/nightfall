856c3cacf03baae4fea91d5776331810c5aca8d7
jameskang105 <jameskang105@gmail.com>
2017-11-15 01:32:48 -0800

    players leaving will now display correct total amount of players

--- src/main/java/deimophobe/nightfall/GameListener.java
+++ src/main/java/deimophobe/nightfall/GameListener.java
@@ -77,7 +77,7 @@ public class GameListener implements Listener {
 	public void onLogoff(PlayerQuitEvent event) {
 		boolean wasDwarf = dm.goOffline(event.getPlayer());
 		mm.goOffline(event.getPlayer());
-		game.unreadyPlayer(event.getPlayer());
+		game.unreadyPlayer(event.getPlayer(), true);
 		if (wasDwarf)
 			game.updateDwarfCount();
 	}
