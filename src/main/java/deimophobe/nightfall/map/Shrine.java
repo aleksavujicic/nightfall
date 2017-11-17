d0c742f2b7c57bce7f7bcde528ab9d89bdf6acbf
Deimophobe <deimophobe@gmail.com>
2017-11-17 20:47:27 +1300

    Reverted shrine fall armour change

--- src/main/java/deimophobe/nightfall/map/Shrine.java
+++ src/main/java/deimophobe/nightfall/map/Shrine.java
@@ -245,13 +245,8 @@ public class Shrine {
 		
 		for (Dwarf dwarf : DwarfManager.getManager().getDwarves()) {
 			dwarf.giveProc(ProcType.SHRINE_FALL);
-			if (shrineProtection.containsPlayer(dwarf) || shrineRegion.containsPlayer(dwarf)) {
-				dwarf.getArmour().repair(1000);
-				dwarf.regenMana(500);
-			} else {
-				dwarf.getArmour().repair(500);
-				dwarf.regenMana(100);
-			}
+			dwarf.getArmour().repair(1000);
+			dwarf.regenMana(200);
 		}
 		for (MonsterPlayer monster : MonsterManager.getManager().getAlivePlayerMobs()) {
 			monster.givePotionEffect(PotionEffectType.SLOW, 180, 3, true, false, true);
