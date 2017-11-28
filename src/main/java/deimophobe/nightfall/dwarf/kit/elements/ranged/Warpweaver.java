6fd136ceaee2b7c2643fd4a0756c005bedbd6b5b
Deimophobe <deimophobe@gmail.com>
2017-11-28 23:25:54 +1300

    Warpweaver costs arrows instead of being cooldown based

--- src/main/java/deimophobe/nightfall/dwarf/kit/elements/ranged/Warpweaver.java
+++ src/main/java/deimophobe/nightfall/dwarf/kit/elements/ranged/Warpweaver.java
@@ -1,10 +1,8 @@
 package deimophobe.nightfall.dwarf.kit.elements.ranged;
 
 import deimophobe.nightfall.ArrowMisc;
-import deimophobe.nightfall.cooldown.ComplexCooldown;
 import deimophobe.nightfall.damage.DwarfDamage;
 import deimophobe.nightfall.dwarf.Dwarf;
-import deimophobe.nightfall.dwarf.kit.KitCooldownElement;
 import deimophobe.nightfall.items.CustomItem;
 import deimophobe.nightfall.map.GameMap;
 import deimophobe.nightfall.monster.MonsterPlayer;
@@ -15,7 +13,6 @@ import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Projectile;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
 import java.util.HashSet;
@@ -24,57 +21,40 @@ import java.util.Set;
 /**
  * Created by Deimophobe on 20/01/17.
  */
-public class Warpweaver extends AbstractToggleBow implements KitCooldownElement {
+public class Warpweaver extends AbstractToggleBow {
 	public Warpweaver(Dwarf dwarf) {
 		super(dwarf);
 	}
+	
+	// Includes cost of fired arrow (so with ARROW_COST = 15,
+	// this will drain 14 arrows on land as 1 arrow was used to fire).
+	private final static int ARROW_COST = 15;
 	
 	private final static int POWER = 30;
 	private final static CustomItem ITEM = getBow("warpweaver", POWER);
 	@Override public CustomItem getItem() {
 		return ITEM;
 	}
-	@Override public ItemStack getCooldownToggleItem() {return ITEM.createItemStack(); }
 	@Override public String getBowIdentifier() {return "WARPBOW";}
 	@Override public int getPower() {return POWER;}
 	
-	/*
-	private Location warpSpot;
-	private boolean warping = false;
-	private int cooldown = 0;
-	
-	private final static int TELEPORT_TIME = 20*20;
-	private final static int MAX_COOLDOWN = 20*20;
-	*/
-	
-	private final ComplexCooldown cooldown = new ComplexCooldown(40 * 20);
 	private final Set<Arrow> activeArrows = new HashSet<>();
 	
 	@Override
-	public void update(boolean quartSec, boolean halfSec, boolean sec, boolean doubleSec, boolean quadSec) {
-		cooldown.update();
-	}
-	
-	@Override
-	public float fractionComplete() {
-		return cooldown.fractionComplete();
-	}
-	
-	@Override
 	public void onProjectileLand(Projectile proj, Block hitBlock) {
-		if (cooldown.isAvailable() && isActive() && isActiveProjectile(proj)) {
-			if (!GameMap.getCurrentMap().getCurrentMobProtection().continsEntity(proj)) {
-				setActive(false);
-				
-				Location newSpot = proj.getLocation().add(0, 0.25, 0);
-				newSpot.add(proj.getLocation().getDirection().multiply(0.25));
-				newSpot.setDirection(dwarf.getLocation().getDirection());
-				teleportTo(newSpot);
-				
-				cooldown.reset();
-				
-				activeArrows.remove(proj);
-			}
+		if (isActive()
+		      && isActiveProjectile(proj)
+		      && dwarf.hasArrows(ARROW_COST - 1)
+		      && !GameMap.getCurrentMap().getCurrentMobProtection().continsEntity(proj)) {
+			
+			setActive(false);
+			removeActiveArrows();
+			dwarf.useArrows(ARROW_COST - 1);
+			
+			Location newSpot = proj.getLocation().add(0, 0.25, 0);
+			newSpot.add(proj.getLocation().getDirection().multiply(0.25));
+			newSpot.setDirection(dwarf.getLocation().getDirection());
+			teleportTo(newSpot);
 		}
 	}
 	
@@ -87,6 +67,12 @@ public class Warpweaver extends AbstractToggleBow implements KitCooldownElement 
 	@Override
 	public Projectile onBowFire(Projectile arrow, float force) {
 		arrow = super.onBowFire(arrow, force);
+		
+		// Note: dwarf does not use arrow until after fire event.
+		if (!dwarf.hasArrows(ARROW_COST)) {
+			setActive(false);
+		}
+		
 		if (isActive()) {
 			ArrowMisc.setGlowColour((Arrow) arrow, ChatColor.DARK_PURPLE);
 			activeArrows.add((Arrow) arrow);
@@ -96,7 +82,7 @@ public class Warpweaver extends AbstractToggleBow implements KitCooldownElement 
 	
 	@Override
 	protected boolean canActivate() {
-		return cooldown.isAvailable();
+		return dwarf.hasArrows(ARROW_COST);
 	}
 	
 	@Override
