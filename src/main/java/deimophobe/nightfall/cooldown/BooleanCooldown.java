bc8cbd3b0249472c9efc36617d27b0ca2c9b437e
Deimophobe <deimophobe@gmail.com>
2017-12-11 23:18:02 +1300

    Minor changes to shadowblade

---
+++ src/main/java/deimophobe/nightfall/cooldown/BooleanCooldown.java
@@ -0,0 +1,98 @@
+package deimophobe.nightfall.cooldown;
+
+
+//TODO RENAME
+
+import java.util.function.Supplier;
+
+/**
+ * Created by Deimophobe on 2/06/17.
+ */
+public class BooleanCooldown implements Cooldown{
+	private int maxCD;
+	private int cd;
+	
+	private final Supplier<Boolean> useAction;
+	private final Runnable cooldownAction;
+	
+	public BooleanCooldown(int maxCD) {
+		this(maxCD, null, null);
+	}
+	
+	public BooleanCooldown(int maxCD, Supplier<Boolean> useAction) {
+		this(maxCD, useAction, null);
+	}
+	
+	public BooleanCooldown(int maxCD, Supplier<Boolean> useAction, Runnable cooldownAction) {
+		this.cd = -1;
+		this.maxCD = maxCD;
+		
+		this.useAction = (useAction == null ? () -> true : useAction);
+		this.cooldownAction = (cooldownAction == null ? () -> {} : cooldownAction);
+	}
+
+	public int getMaxCD() {
+		return maxCD;
+	}
+
+	public void setMaxCD(int newCD) {
+		maxCD = newCD;
+	}
+
+	public void update() {
+		if (cd > 0)
+			cd--;
+		
+		if (cd == 0) {
+			cd = -1;
+			cooldownAction.run();
+		}
+	}
+	
+	public boolean isAvailable() {
+		return cd == -1;
+	}
+	
+	public boolean tryUse() {
+		if (isAvailable()) {
+			boolean success = useAction.get();
+			if (success) {
+				reset();
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+	
+	public void reduceCooldown(int amt) {
+		if (isAvailable()) return;
+		
+		cd -= amt;
+		if (cd <= 0) {
+			cooldownAction.run();
+			cd = -1;
+		}
+	}
+	
+	public void reset() {
+		cd = maxCD;
+	}
+	
+	public void stop() {
+		cd = -1;
+	}
+	
+	public float fractionComplete() {
+		if (cd == -1)
+			return 1;
+		else
+			return 1 - (float) cd/maxCD;
+	}
+	
+	public boolean wasUsedWithin(int time) {
+		return (cd >= maxCD - time);
+	}
+}
