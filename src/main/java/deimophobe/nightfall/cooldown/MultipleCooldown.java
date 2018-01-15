83890c671898765bf07c24751493be575089811e
Deimophobe <deimophobe@gmail.com>
2018-01-15 16:27:04 +1300

    Changed to silvetris claws

---
+++ src/main/java/deimophobe/nightfall/cooldown/MultipleCooldown.java
@@ -0,0 +1,93 @@
+package deimophobe.nightfall.cooldown;
+
+/**
+ * Created by Deimophobe on 15/01/18.
+ */
+public class MultipleCooldown implements Cooldown{
+	private int maxCD;
+	private int cd;
+	private final int useCost;
+	
+	private final Runnable useAction;
+	private final Runnable cooldownAction;
+	
+	public static final Runnable DO_NOTHING = () -> {};
+	
+	public MultipleCooldown(int maxCD) {
+		this(maxCD, DO_NOTHING, DO_NOTHING);
+	}
+	
+	public MultipleCooldown(int maxCD, Runnable useAction) {
+		this(maxCD, useAction, DO_NOTHING);
+	}
+	
+	public MultipleCooldown(int maxCD, Runnable useAction, Runnable cooldownAction) {
+		this(maxCD, maxCD, useAction, cooldownAction);
+	}
+	
+	public MultipleCooldown(int maxCD, int useCost, Runnable useAction, Runnable cooldownAction) {
+		this.cd = 0;
+		this.maxCD = maxCD;
+		this.useCost = useCost;
+		
+		this.useAction = (useAction == null ? DO_NOTHING : useAction);
+		this.cooldownAction = (cooldownAction == null ? DO_NOTHING : cooldownAction);
+	}
+	
+	public int getMaxCD() {
+		return maxCD;
+	}
+	
+	public void setMaxCD(int newCD) {
+		maxCD = newCD;
+	}
+	
+	public void update() {
+		if (cd > 0) {
+			cd--;
+			
+			if (cd == 0) cooldownAction.run();
+		}
+	}
+	
+	public boolean isAvailable() {
+		return cd + useCost <= maxCD;
+	}
+	
+	public boolean tryUse() {
+		if (isAvailable()) {
+			useAction.run();
+			cd = Math.min(cd + useCost, maxCD);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void reduceCooldown(int amt) {
+		if (isAvailable()) return;
+		
+		cd -= amt;
+		if (cd <= 0) {
+			cooldownAction.run();
+			cd = 0;
+		}
+	}
+	
+	public void reset() {
+		cd = maxCD;
+	}
+	
+	public void stop() {
+		cd = 0;
+	}
+	
+	public float fractionComplete() {
+		return 1 - (float) cd/maxCD;
+	}
+	
+	public boolean wasUsedWithin(int time) {
+		return (cd >= maxCD - time);
+	}
+}
+
--- src/main/java/deimophobe/nightfall/dwarf/kit/elements/hero/lyra/SilvetrisClaws.java
+++ src/main/java/deimophobe/nightfall/dwarf/kit/elements/hero/lyra/SilvetrisClaws.java
@@ -2,7 +2,7 @@ package deimophobe.nightfall.dwarf.kit.elements.hero.lyra;
 
 import deimophobe.nightfall.common.Misc;
 import deimophobe.nightfall.common.items.CustomItem;
-import deimophobe.nightfall.cooldown.ComplexCooldown;
+import deimophobe.nightfall.cooldown.MultipleCooldown;
 import deimophobe.nightfall.dwarf.Dwarf;
 import deimophobe.nightfall.dwarf.DwarvenItems;
 import deimophobe.nightfall.dwarf.kit.KitCooldownElement;
@@ -20,7 +20,8 @@ public class SilvetrisClaws extends AbstractItem implements KitCooldownElement {
 	
 	public SilvetrisClaws(Dwarf dwarf) { super(dwarf); }
 	
-	private final ComplexCooldown leapCD = new ComplexCooldown(1, this::leap);
+	private boolean hasLanded = true;
+	private final MultipleCooldown leapCD = new MultipleCooldown(60*20, 20*20, this::leap, null);
 	
 	private final static CustomItem ITEM = DwarvenItems.getItem("hero", "claw");
 	@Override public CustomItem getItem() {return ITEM; }
@@ -31,19 +32,26 @@ public class SilvetrisClaws extends AbstractItem implements KitCooldownElement {
 	public void update(boolean quartSec, boolean halfSec, boolean sec, boolean doubleSec, boolean quadSec) {
 		super.update(quartSec, halfSec, sec, doubleSec, quadSec);
 		leapCD.update();
+		
+		if (!hasLanded && dwarf.getPlayer().isOnGround()) hasLanded = true;
 	}
 	
 	@Override
 	public boolean onUse(Action action, Block clickedBlock, BlockFace blockFace) {
 		super.onUse(action, clickedBlock, blockFace);
-		if (Misc.isRightClick(action)) {
+		if (Misc.isRightClick(action) && hasLanded) {
 			return leapCD.tryUse();
 		}
 		return false;
 	}
 	
 	private void leap() {
-		dwarf.leap(3, 1);
+		hasLanded = false;
+		if (dwarf.isSneaking()) {
+			dwarf.leap(1, 2);
+		} else {
+			dwarf.leap(3, 1);
+		}
 	}
 	
 	@Override
