8f4d774db266f09816167a516f819f45df3c6043
Deimophobe <deimophobe@gmail.com>
2018-01-18 18:49:36 +1300

    Changes to tombmaker reward system

--- src/main/java/deimophobe/nightfall/dwarf/kit/elements/accessory/Tombmaker.java
+++ src/main/java/deimophobe/nightfall/dwarf/kit/elements/accessory/Tombmaker.java
@@ -14,6 +14,8 @@ import deimophobe.nightfall.dwarf.kit.KitCooldownElement;
 import deimophobe.nightfall.dwarf.kit.KitGiveType;
 import deimophobe.nightfall.dwarf.kit.elements.DwarfShovel;
 import deimophobe.nightfall.dwarf.kit.elements.KitElementType;
+import deimophobe.nightfall.util.Weightable;
+import deimophobe.nightfall.util.WeightedSet;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -21,6 +23,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -69,14 +72,15 @@ public class Tombmaker extends DwarfShovel implements KitCooldownElement {
 	}
 	
 	
-	private static final double FIND_CHANCE = 0.001;
+	private static final double FIND_CHANCE = 0.0005;
 	
 	@Override
 	public void onBlockBreak(Block block, boolean didBreak) {
 		super.onBlockBreak(block, didBreak);
 		if (block.getType() == Material.GRAVEL && Game.getGame().getPhase() == Phase.BUILD && didBreak) {
-			if (Math.random() <= FIND_CHANCE) {
-				ScavengeItem.getRandom().giveToDwarf(dwarf);
+			double chance = FIND_CHANCE * (dwarf.hasPotionEffect(PotionEffectType.FAST_DIGGING) ? 2 : 1);
+			if (Math.random() <= chance) {
+				REWARD_TIERS.getRandom().getRandomItem().rewardDwarf(dwarf);
 			}
 		}
 	}
@@ -86,53 +90,50 @@ public class Tombmaker extends DwarfShovel implements KitCooldownElement {
 		return hasteCD.fractionComplete();
 	}
 	
-	private static class ScavengeItem {
-		private static double MAX_WEIGHT;
-		private static final Set<ScavengeItem> ITEMS = new HashSet<>();
-		static {
-			new ScavengeItem(ConsumableType.SLAB, 1, 5, "slab");
-			new ScavengeItem(ConsumableType.SLAB, 2, 3, "slabs");
-			new ScavengeItem(ConsumableType.SLAB, 3, 1, "slabs");
-			new ScavengeItem(ConsumableType.HEAL_STATION, 2, 5, "healing stations");
-			new ScavengeItem(ConsumableType.HEAL_STATION, 4, 2, "healing stations");
-			new ScavengeItem(ConsumableType.HEAL_STATION, 8, 0.5, "healing stations");
-			new ScavengeItem(ConsumableType.LAMP, 16, 5, "lamps");
-			new ScavengeItem(ConsumableType.LAMP, 32, 2, "lamps");
-			new ScavengeItem(ConsumableType.LAMP, 64, 1, "lamps");
-			new ScavengeItem(ConsumableType.SOS, 1, 1.5, "sos");
-			new ScavengeItem(ConsumableType.SOS, 2, 0.5, "sos");
-			new ScavengeItem(ConsumableType.WRENCH, 1, 2, "wrench");
-			new ScavengeItem(ConsumableType.WRENCH, 2, 0.5, "wrenches");
-			new ScavengeItem(ConsumableType.WIZARD_MORTAR, 16, 5, "wizard mortar");
-			new ScavengeItem(ConsumableType.WIZARD_MORTAR, 32, 2, "wizard mortar");
-			new ScavengeItem(ConsumableType.WIZARD_MORTAR, 64, 1, "wizard mortar");
+	
+	
+	
+	static {
+		new ConsumableScavengeItem(ConsumableType.SLAB, 1, "slab", RewardTier.COMMON);
+		new ConsumableScavengeItem(ConsumableType.SLAB, 2, "slabs", RewardTier.UNCOMMON);
+		new ConsumableScavengeItem(ConsumableType.SLAB, 3, "slabs", RewardTier.RARE);
+		
+		new ConsumableScavengeItem(ConsumableType.HEAL_STATION, 2, "healing stations", RewardTier.COMMON);
+		new ConsumableScavengeItem(ConsumableType.HEAL_STATION, 4, "healing stations", RewardTier.UNCOMMON);
+		new ConsumableScavengeItem(ConsumableType.HEAL_STATION, 8, "healing stations", RewardTier.RARE);
+		
+		new ConsumableScavengeItem(ConsumableType.LAMP, 32, "lamps", RewardTier.COMMON);
+		new ConsumableScavengeItem(ConsumableType.LAMP, 64, "lamps", RewardTier.UNCOMMON);
+		
+		new ConsumableScavengeItem(ConsumableType.SOS, 1, "swords of santification", RewardTier.UNCOMMON);
+		new ConsumableScavengeItem(ConsumableType.SOS, 2, "swords of santification", RewardTier.RARE);
+		new ConsumableScavengeItem(ConsumableType.SOS, 4, "swords of santification", RewardTier.LEGENDARY);
+		
+		new ConsumableScavengeItem(ConsumableType.WRENCH, 1, "wrench", RewardTier.COMMON);
+		new ConsumableScavengeItem(ConsumableType.WRENCH, 2, "wrenches", RewardTier.UNCOMMON);
+		new ConsumableScavengeItem(ConsumableType.WRENCH, 3, "wrenches", RewardTier.RARE);
+		
+		new ConsumableScavengeItem(ConsumableType.WIZARD_MORTAR, 32, "wizard mortar", RewardTier.UNCOMMON);
+		
+		new ConsumableScavengeItem(ConsumableType.PROC_BOTTLE, 2, "proc in a bottles", RewardTier.COMMON);
+		new ConsumableScavengeItem(ConsumableType.PROC_BOTTLE, 3, "proc in a bottles", RewardTier.UNCOMMON);
+		new ConsumableScavengeItem(ConsumableType.PROC_BOTTLE, 5, "proc in a bottles", RewardTier.RARE);
+		
+		new FixedScavengeItem("perfect-torch", "The Perfect Torch");
+	}
+	
+	private static abstract class ScavengeItem {
+		private final RewardTier tier;
+		
+		private ScavengeItem(RewardTier tier) {
+			this.tier = tier;
 			
-			for (ScavengeItem item : ITEMS)
-				item.computeAdjWeight();
+			tier.addItem(this);
 		}
 		
-		private final ConsumableType type;
-		private final int amt;
-		private final double rawWeight;
-		private final String displayName;
-		private double adjustedWeight;
-		
-		private ScavengeItem(ConsumableType type, int amt, double rawWeight, String displayName) {
-			this.type = type;
-			this.amt = amt;
-			this.rawWeight = rawWeight;
-			
-			MAX_WEIGHT += rawWeight;
-			this.displayName = displayName;
-			ITEMS.add(this);
-		}
-		
-		private void computeAdjWeight() {
-			adjustedWeight = rawWeight/MAX_WEIGHT;
-		}
-		
-		private void giveToDwarf(Dwarf dwarf) {
-			dwarf.giveConsumable(type, amt);
+		private void rewardDwarf(Dwarf dwarf) {
+			giveItemToDwarf(dwarf);
+			tier.onDwarfReward(dwarf);
 			
 			// SHOW PARTICLES!
 			Location bodyCentre = dwarf.getEyeLocation().add(0, -0.5, 0);
@@ -150,21 +151,105 @@ public class Tombmaker extends DwarfShovel implements KitCooldownElement {
 				}
 			}
 			
-			Bukkit.broadcastMessage(dwarf.getDisplayName() + ChatColor.YELLOW + " has found " +
-					ChatColor.GREEN + amt + " " + displayName + ChatColor.YELLOW + "!");
+			Bukkit.broadcastMessage(
+					dwarf.getDisplayName()
+							+ ChatColor.YELLOW + " has found "
+							+ tier.colour + getDisplayName()
+							+ ChatColor.YELLOW + "!"
+			);
 			
-			dwarf.playSound("entity.player.levelup", 2f, 0.6f, true);
+			dwarf.playSound("entity.player.levelup", 1f, 0.6f, true);
 		}
 		
-		private static ScavengeItem getRandom() {
-			double rand = Math.random();
-			for (ScavengeItem item : ITEMS) {
-				rand -= item.adjustedWeight;
-				if (rand < 0)
-					return item;
-			}
-			// Should never reach here
-			return ITEMS.iterator().next();
+		abstract void giveItemToDwarf(Dwarf dwarf);
+		abstract String getDisplayName();
+		
+	}
+	
+	private static class ConsumableScavengeItem extends ScavengeItem {
+		private final ConsumableType type;
+		private final int amt;
+		private final String displayName;
+		
+		private ConsumableScavengeItem(ConsumableType type, int amt, String displayName, RewardTier tier) {
+			super(tier);
+			this.type = type;
+			this.amt = amt;
+			this.displayName = displayName;
 		}
+		
+		@Override
+		void giveItemToDwarf(Dwarf dwarf) {
+			dwarf.giveConsumable(type, amt);
+		}
+		
+		@Override
+		String getDisplayName() {
+			return amt + " " + displayName;
+		}
+	}
+	
+	private static class FixedScavengeItem extends ScavengeItem {
+		private final ItemStack item;
+		private final String displayName;
+		
+		private FixedScavengeItem(String itemName, String displayName) {
+			super(RewardTier.LEGENDARY);
+			this.item = DwarvenItems.getItem("misc", itemName).createItemStack();
+			this.displayName = displayName;
+		}
+		
+		@Override
+		void giveItemToDwarf(Dwarf dwarf) {
+			dwarf.giveItem(item);
+		}
+		
+		@Override
+		String getDisplayName() {
+			return displayName;
+		}
+	}
+	
+	private static final WeightedSet<RewardTier> REWARD_TIERS = new WeightedSet<>();
+	static {
+		REWARD_TIERS.addAll(EnumSet.allOf(RewardTier.class));
+	}
+	private enum RewardTier implements Weightable {
+		COMMON(60, ChatColor.GREEN),
+		UNCOMMON(30, ChatColor.BLUE),
+		RARE(10, ChatColor.DARK_PURPLE),
+		
+		LEGENDARY(0.5, ChatColor.GOLD) {
+			@Override
+			void onDwarfReward(Dwarf dwarf) {
+				dwarf.makePlagueImmune();
+			}
+		}
+		
+		;
+		
+		private final double weight;
+		private final ChatColor colour;
+		private final Set<ScavengeItem> items = new HashSet<>();
+		
+		RewardTier(double weight, ChatColor colour) {
+			this.weight = weight;
+			this.colour = colour;
+		}
+		
+		private void addItem(ScavengeItem item) {
+			items.add(item);
+		}
+		
+		private ScavengeItem getRandomItem() {
+			return Misc.getRandom(items);
+		}
+		
+		@Override
+		public double getWeight() {
+			return weight;
+		}
+		
+		void onDwarfReward(Dwarf dwarf) {};
 	}
 }
