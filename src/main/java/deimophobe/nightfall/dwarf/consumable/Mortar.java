b73934fc1fe5e57b6f4506fc1683f19f3e1e2c40
Deimophobe <deimophobe@gmail.com>
2018-01-18 17:50:50 +1300

    Mortar oopsie

--- src/main/java/deimophobe/nightfall/dwarf/consumable/Mortar.java
+++ src/main/java/deimophobe/nightfall/dwarf/consumable/Mortar.java
@@ -29,7 +29,7 @@ class Mortar extends Consumable {
 		
 		if (clickedBlock == null)
 			clickedBlock = dwarf.getPlayer().getTargetBlock((Set<Material>) null, 5);
-		boolean shouldWizzy = wizzy || Game.getGame().getPhase().hasGameStarted();
+		boolean shouldWizzy = wizzy || !Game.getGame().getPhase().hasGameStarted();
 		
 		BlockConverter.mortar(clickedBlock, shouldWizzy);
 		
