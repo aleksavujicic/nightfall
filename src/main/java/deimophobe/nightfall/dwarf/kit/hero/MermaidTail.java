7ae74bf8e81a2e2c5ce5ce74c6e272fc34289063
Deimophobe <deimophobe@gmail.com>
2018-01-27 17:02:26 +1300

    Herana swims fast automatically

--- src/main/java/deimophobe/nightfall/dwarf/kit/hero/MermaidTail.java
+++ src/main/java/deimophobe/nightfall/dwarf/kit/hero/MermaidTail.java
@@ -10,7 +10,6 @@ import org.bukkit.potion.PotionEffectType;
  * Created by Deimophobe on 14/01/18.
  */
 public class MermaidTail extends AbstractPiece implements ArmourPiece {
-	private boolean fastSwimming = false;
 	
 	public MermaidTail(Dwarf dwarf) {
 		super(dwarf);
@@ -27,8 +26,9 @@ public class MermaidTail extends AbstractPiece implements ArmourPiece {
 	public void update(boolean quartSec, boolean halfSec, boolean sec, boolean doubleSec, boolean quadSec) {
 		super.update(quartSec, halfSec, sec, doubleSec, quadSec);
 		
-		if (quartSec && fastSwimming && !dwarf.isUnderwater()) {
-			removeFastSwim();
+		if (quartSec) {
+			if (dwarf.isUnderwater()) giveFastSwim();
+			else removeFastSwim();
 		}
 	}
 	
@@ -37,22 +37,15 @@ public class MermaidTail extends AbstractPiece implements ArmourPiece {
 		super.onShift(sneaking);
 		if (!sneaking) return;
 		dwarf.givePermanentPotionEffect(PotionEffectType.WATER_BREATHING, 1);
-		
-		if (dwarf.isUnderwater()) {
-			if (!fastSwimming) giveFastSwim();
-			else removeFastSwim();
-		}
 	}
 	
 	private void giveFastSwim() {
 		dwarf.givePermanentPotionEffect(PotionEffectType.SPEED, 8);
 		dwarf.givePermanentPotionEffect(PotionEffectType.NIGHT_VISION, 1);
-		fastSwimming = true;
 	}
 	
 	private void removeFastSwim() {
 		dwarf.removePotionEffect(PotionEffectType.SPEED);
 		dwarf.removePotionEffect(PotionEffectType.NIGHT_VISION);
-		fastSwimming = false;
 	}
 }
