e964320a26487d20edb149787e45f8b8a4708237
Deimophobe <deimophobe@gmail.com>
2018-02-19 22:02:53 +1300

    Added a util sorting method

--- src/main/java/deimophobe/nightfall/common/Misc.java
+++ src/main/java/deimophobe/nightfall/common/Misc.java
@@ -48,6 +48,14 @@ public class Misc {
 		return min + (Math.random() * (max - min));
 	}
 	
+	
+	
+	public static <T extends Comparable<? super T>> List<T> asSortedList(Collection<T> c) {
+		List<T> list = new ArrayList<T>(c);
+		java.util.Collections.sort(list);
+		return list;
+	}
+	
 	// ------ ACTION CLICK -------
 	
 	public static boolean isLeftClick(Action type) {
