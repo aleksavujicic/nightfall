c17924bfa10dcf65b8cd81b70de7ec6dd9981acf
Deimophobe <deimophobe@gmail.com>
2018-02-23 22:30:17 +1300

    Less mele knockback

--- src/main/java/deimophobe/nightfall/damage/GameDamage.java
+++ src/main/java/deimophobe/nightfall/damage/GameDamage.java
@@ -119,9 +119,9 @@ public abstract class GameDamage<A extends GameEntity, R extends GameEntity> imp
 		if (itemStack != null) knockbackLevel = itemStack.getEnchantmentLevel(Enchantment.KNOCKBACK);
 		
 		Vector offset = receiver.offsetFrom(attacker);
-		offset.setY(0).normalize().multiply(0.6 + 0.3 * knockbackLevel);
+		offset.setY(0).normalize().multiply(0.4 + 0.3 * knockbackLevel);
 		offset.setY(0.25);
-		offset.add(attacker.getVelocity().multiply(0.7));
+		offset.add(attacker.getVelocity().multiply(0.5));
 		
 		knockback = offset;
 	}
