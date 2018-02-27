8e695c88f5a4b10952f9860fc6724e825edd34e9
Deimophobe <deimophobe@gmail.com>
2018-02-27 18:31:13 +1300

    Minor kb change

--- src/main/java/deimophobe/nightfall/damage/GameDamage.java
+++ src/main/java/deimophobe/nightfall/damage/GameDamage.java
@@ -122,7 +122,7 @@ public abstract class GameDamage<A extends GameEntity, R extends GameEntity> imp
 		Vector offset = receiver.offsetFrom(attacker);
 		offset.setY(0).normalize().multiply(0.5 + 0.25 * knockbackLevel);
 		offset.setY(0.35 + 0.05 * knockbackLevel);
-		offset.add(attacker.getVelocity().multiply(0.5));
+		offset.add(attacker.getVelocity().setY(0).multiply(0.5));
 		
 		knockback = offset;
 	}
