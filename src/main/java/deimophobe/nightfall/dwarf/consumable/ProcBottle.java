c51c181e97115e34428577c77bd0a839263fea6a
Deimophobe <deimophobe@gmail.com>
2018-03-03 16:36:37 +1300

    Proc in a bottle texture updated

--- src/main/java/deimophobe/nightfall/dwarf/consumable/ProcBottle.java
+++ src/main/java/deimophobe/nightfall/dwarf/consumable/ProcBottle.java
@@ -3,12 +3,10 @@ package deimophobe.nightfall.dwarf.consumable;
 import deimophobe.nightfall.common.Misc;
 import deimophobe.nightfall.dwarf.Dwarf;
 import deimophobe.nightfall.dwarf.ProcType;
-import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.block.Action;
-import org.bukkit.material.MaterialData;
 
 /**
  * Created by Deimophobe on 9/01/18.
@@ -26,7 +24,7 @@ public class ProcBottle extends Consumable {
 		
 		dwarf.giveProc(ProcType.PROC_BOTTLE);
 		dwarf.playSound("block.glass.break", 1f, 1f, true);
-		dwarf.getWorld().spawnParticle(Particle.BLOCK_CRACK, dwarf.getEyeLocation(), 30, 0.5, 0.5, 0.5, 0, new MaterialData(Material.GLASS));
+		dwarf.getWorld().spawnParticle(Particle.ITEM_CRACK, dwarf.getEyeLocation(), 30, 0.5, 0.5, 0.5, 0, getItemStack());
 		return DEFAULT_CD;
 	}
 }
