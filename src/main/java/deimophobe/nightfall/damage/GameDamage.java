6907615c5e261756f7e5d6f34c249ac145d86fb0
Deimophobe <deimophobe@gmail.com>
2018-03-10 14:35:39 +1300

    damage.willKill() has some error tolerance

--- src/main/java/deimophobe/nightfall/damage/GameDamage.java
+++ src/main/java/deimophobe/nightfall/damage/GameDamage.java
@@ -249,7 +249,7 @@ public abstract class GameDamage<A extends GameEntity, R extends GameEntity> imp
 	}
 	
 	public boolean willKill() {
-		return (receiver.getHealth() - getFinalDamage() <= 0 || instaKill);
+		return (receiver.getHealth() - getFinalDamage() <= 0.000001 || instaKill);
 	}
 	
 	public boolean hasArrow() {return  projectile instanceof Arrow;}
