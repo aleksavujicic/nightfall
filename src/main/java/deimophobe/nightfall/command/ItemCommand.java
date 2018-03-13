f845902a88da51e5512ecbc7db91afb8060c4460
Deimophobe <deimophobe@gmail.com>
2018-03-14 00:19:35 +1300

    I fixed the command message i was supposed to fix

--- src/main/java/deimophobe/nightfall/command/ItemCommand.java
+++ src/main/java/deimophobe/nightfall/command/ItemCommand.java
@@ -4,8 +4,8 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.*;
 import co.aikar.commands.contexts.OnlinePlayer;
 import deimophobe.nightfall.common.items.CustomItem;
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 /**
@@ -18,10 +18,11 @@ public class ItemCommand extends BaseCommand {
 	@CommandCompletion("@players @items")
 	@Description("Give an item to a player.")
 	public void giveItem(CommandSender sender, OnlinePlayer player, @Flags("all") CustomItem item, @Default("1") int quantity) {
+		Player realPlayer = player.getPlayer();
 		ItemStack stack = item.createItemStack();
 		stack.setAmount(quantity);
-		player.getPlayer().getInventory().addItem(stack);
+		realPlayer.getInventory().addItem(stack);
 		
-		sender.sendMessage(ChatColor.YELLOW + "deimo fix this message");
+		MessageUtil.sendMessage(sender, "Gave ", quantity, " of item to ", realPlayer);
 	}
 }
