e1ed5b1e1e697da90c01a7ea7ead5610f01d0fb9
TKiwisi <jameskang105@gmail.com>
2018-03-14 21:02:31 -0700

    minotaur charge buffed slightly

--- src/main/java/deimophobe/nightfall/monster/mob/Minotaur.java
+++ src/main/java/deimophobe/nightfall/monster/mob/Minotaur.java
@@ -118,8 +118,8 @@ public class Minotaur extends AbstractMob {
 	}
 	
 	private static final double AOE_RADIUS = 2.5;
-	private static final int AOE_DMG = 60; // This is a one off hit so its not as strong as it seems.
-	private static final int AOE_SHRED = 25;
+	private static final int AOE_DMG = 80; // This is a one off hit so its not as strong as it seems.
+	private static final int AOE_SHRED = 50;
 	private void aoeDamage() {
 		/*
 		DamageManager.getManager().AOEDamage(DwarfManager.getManager().getDwarves(), monster,
