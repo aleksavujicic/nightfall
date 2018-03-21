d7586e2f8d268e3641f7aaec941fe0f7387649c9
Deimophobe <deimophobe@gmail.com>
2018-03-22 01:22:42 +1300

    Misc.randomInt is no longer top exclusive

--- src/main/java/deimophobe/nightfall/common/Misc.java
+++ src/main/java/deimophobe/nightfall/common/Misc.java
@@ -41,7 +41,7 @@ public class Misc {
 	}
 	
 	public static int randomInt(int min, int max) {
-		return min + (int) (Math.random() * (max - min));
+		return min + (int) (Math.random() * (max + 1 - min));
 	}
 	
 	public static double randomDouble(double min, double max) {
