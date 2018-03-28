32c03f09275167a33805b0de9592ecc478fdec0a
Deimophobe <deimophobe@gmail.com>
2018-03-28 23:09:01 +1300

    Regrowth does AOE heal/regen buff

--- src/main/java/deimophobe/nightfall/dwarf/kit/healing/Regrowth.java
+++ src/main/java/deimophobe/nightfall/dwarf/kit/healing/Regrowth.java
@@ -6,11 +6,11 @@ import deimophobe.nightfall.common.items.CustomItem;
 import deimophobe.nightfall.cooldown.ComplexCooldown;
 import deimophobe.nightfall.dwarf.Dwarf;
 import deimophobe.nightfall.dwarf.DwarfManager;
-import deimophobe.nightfall.plague.AssassinPlague;
 import org.bukkit.Particle;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.block.Action;
+import org.bukkit.potion.PotionEffectType;
 
 /**
  * Created by Deimophobe on 22/01/17.
@@ -55,7 +55,25 @@ public class Regrowth extends AbstractAle {
 		healOthersCD.update();
 		targetClearer.update();
 	}
-
+	
+	private static final double AOE_REGEN_RANGE = 5;
+	@Override
+	protected void heal() {
+		super.heal();
+		
+		dwarf.givePotionEffect(PotionEffectType.REGENERATION, 10*20, 3, true, false, false);
+		dwarf.playSound("healing", 0.5f, 2f, false);
+		
+		for (Dwarf other : DwarfManager.getManager().getDwarves()) {
+			if (other == dwarf) continue;
+			if (other.distanceTo(dwarf) > AOE_REGEN_RANGE) continue;
+			
+			other.heal(8);
+			other.givePotionEffect(PotionEffectType.REGENERATION, 5*20, 1, true, false, false);
+			other.playSound("healing", 0.5f, 2f, false);
+		}
+	}
+	
 	private void tryHealOthers() {
 		if (target == null || dwarf.distanceTo(target) > 20)
 			target = dwarf.getLookingAt(20, 3, DwarfManager.getManager().getDwarves());
