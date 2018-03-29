4ba13723586fd5f0ca35dcaf372e0263ac9332d7
TKiwisi <jameskang105@gmail.com>
2018-03-29 02:01:33 -0700

    flamelancers can set people on fire

--- src/main/java/deimophobe/nightfall/monster/mob/SkeletonFlamelancer.java
+++ src/main/java/deimophobe/nightfall/monster/mob/SkeletonFlamelancer.java
@@ -4,6 +4,7 @@ import deimophobe.nightfall.blocks.blocktype.BlockType;
 import deimophobe.nightfall.common.Misc;
 import deimophobe.nightfall.common.items.modifiers.ItemModifierType;
 import deimophobe.nightfall.damage.DwarfDamage;
+import deimophobe.nightfall.damage.GameDamageType;
 import deimophobe.nightfall.damage.MonsterDamage;
 import deimophobe.nightfall.monster.MonsterPlayer;
 import deimophobe.nightfall.monster.SpawnMethod;
@@ -118,6 +119,9 @@ class SkeletonFlamelancer extends Skeleton {
 	public void onDamageAttack(DwarfDamage damage) {
 		super.onDamageAttack(damage);
 		damage.getDwarf().getPlayer().setFireTicks(30 + flame*10);
+		if (damage.getType() == GameDamageType.MELEE) {
+			damage.getDwarf().getPlayer().setFireTicks(40);
+		}
 	}
 
 	@Override
