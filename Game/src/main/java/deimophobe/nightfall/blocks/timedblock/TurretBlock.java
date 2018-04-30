be409ecfef49c53019ceee3b588e9822b6b87a64
Deimophobe <deimophobe@gmail.com>
2018-05-01 00:46:28 +1200

    Turret place sound effects

--- Game/src/main/java/deimophobe/nightfall/blocks/timedblock/TurretBlock.java
+++ Game/src/main/java/deimophobe/nightfall/blocks/timedblock/TurretBlock.java
@@ -4,13 +4,13 @@ import deimophobe.nightfall.blocks.blocktype.BlockType;
 import deimophobe.nightfall.dwarf.Dwarf;
 import deimophobe.nightfall.dwarf.consumable.ConsumableType;
 import deimophobe.nightfall.util.ArrowMisc;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
 import org.bukkit.entity.Arrow;
 import org.bukkit.material.Dispenser;
+import org.bukkit.material.MaterialData;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
@@ -36,7 +36,7 @@ public class TurretBlock extends DataTimedBlock {
 		Vector offset = adjacentBlock.getLocation().subtract(block.getLocation()).toVector();
 		
 		Location center = block.getLocation().add(0.5, 0.5, 0.5);
-		this.launchLocation = center.add(offset.clone().multiply(0.7));
+		this.launchLocation = center.add(offset.clone().multiply(0.5));
 		launchLocation.setDirection(offset);
 	}
 	
@@ -53,6 +53,27 @@ public class TurretBlock extends DataTimedBlock {
 		dispenser.setFacingDirection(face);
 		state.setData(dispenser);
 		state.update();
+	}
+	
+	@Override
+	public void placeBlock() {
+		super.placeBlock();
+		Location center = block.getLocation().add(0.5, 0.5, 0.5);
+		World world = center.getWorld();
+		world.spawnParticle(Particle.BLOCK_CRACK, center, 20, 0.3, 0.4, 0.4, 0, new MaterialData(Material.ANVIL));
+		world.playSound(center, Sound.BLOCK_METAL_PLACE, 1f, 1f);
+		world.playSound(center, Sound.BLOCK_ANVIL_PLACE, 0.8f, 0.8f);
+	}
+	
+	@Override
+	public void unplaceBlock(boolean cancelled) {
+		super.unplaceBlock(cancelled);
+		if (cancelled) return;
+		
+		Location center = block.getLocation().add(0.5, 0.5, 0.5);
+		World world = center.getWorld();
+		world.spawnParticle(Particle.BLOCK_CRACK, center, 15, 0.4, 0.4, 0.4, 0, block.getState().getData());
+		world.playSound(center, Sound.BLOCK_STONE_PLACE, 1f, 1f);
 	}
 	
 	@Override
