36ad2fd86324801f4e412f7ed9be8913bc5561c9
Deimophobe <deimophobe@gmail.com>
2018-05-02 20:59:06 +1200

    Soulblade particle improvements + gets res with higher stacks

--- Game/src/main/java/deimophobe/nightfall/dwarf/kit/melee/Soulblade.java
+++ Game/src/main/java/deimophobe/nightfall/dwarf/kit/melee/Soulblade.java
@@ -2,7 +2,9 @@ package deimophobe.nightfall.dwarf.kit.melee;
 
 import deimophobe.nightfall.ClickType;
 import deimophobe.nightfall.common.items.CustomItem;
-import deimophobe.nightfall.cooldown.ComplexCooldown;
+import deimophobe.nightfall.cooldown.Cooldown;
+import deimophobe.nightfall.cooldown.UseCooldown;
+import deimophobe.nightfall.damage.DwarfDamage;
 import deimophobe.nightfall.damage.GameDamageType;
 import deimophobe.nightfall.damage.MonsterDamage;
 import deimophobe.nightfall.dwarf.Dwarf;
@@ -13,6 +15,7 @@ import deimophobe.nightfall.dwarf.kit.KitGiveType;
 import deimophobe.nightfall.monster.MonsterEntity;
 import deimophobe.nightfall.monster.MonsterManager;
 import deimophobe.nightfall.monster.ai.AIEntity;
+import deimophobe.nightfall.util.Util;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.World;
@@ -22,11 +25,10 @@ import org.bukkit.util.Vector;
 
 public class Soulblade extends AbstractItem implements CooldownPiece {
 
-	private final ComplexCooldown soulShatterCD = new ComplexCooldown(10, this::soulShatter);
+	private final Cooldown soulShatterCD = new UseCooldown(10, this::soulShatter);
 
 	private static final double SOUL_SHATTER_RADIUS = 3.5;
 	private static final int MAX_SOULS = 50;
-	private static final double HIT = 1;
 
 	private double souls = 0;
 
@@ -47,15 +49,32 @@ public class Soulblade extends AbstractItem implements CooldownPiece {
 	public void update() {
 		super.update();
 		soulShatterCD.update();
+		
+		if (isHoldingItem()) {
+			showParticle();
+		}
 	}
 	
 	@Override
 	public void onDamageAttack(MonsterDamage damage) {
 		super.onDamageAttack(damage);
 		if (isMeleeDamageFromItem(damage)) {
-			souls += HIT;
+			if (damage.getReceiver().isAI()) {
+				souls += 0.7;
+			} else {
+				souls += 1;
+			}
 		}
 		soulCheck();
+	}
+	
+	@Override
+	public void onDamageReceive(DwarfDamage damage) {
+		super.onDamageReceive(damage);
+		if (isHoldingItem()) {
+			double resistance = soulScaling(0, 0.3);
+			damage.getMultiPartDamage().timesMult(1 - resistance);
+		}
 	}
 	
 	@Override
@@ -68,30 +87,44 @@ public class Soulblade extends AbstractItem implements CooldownPiece {
 		return false;
 	}
 	
-	/*
 	@Override
-	public void onShift(boolean sneaking) {
-		super.onShift(sneaking);
-		souls++;
-		souls = Math.min(souls, MAX_SOULS);
+	public float getCooldown() {
+		return Math.min(1, (float) souls/MAX_SOULS);
 	}
-	*/
+	
+	
+//	@Override
+//	public void onShift(boolean sneaking) {
+//		super.onShift(sneaking);
+//		souls++;
+//		soulCheck();
+//	}
+	
+	
+	private void soulCheck() {
+		if (souls > MAX_SOULS) {
+			souls = MAX_SOULS;
+		}
+	}
+	
+	private double soulScaling(double min, double max) {
+		return min + (max - min) * souls/MAX_SOULS;
+	}
 	
 	private void soulShatter() {
-		//Location center = dwarf.getLocation().add(dwarf.getLocation().getDirection().multiply(1.5));
 		Location center = dwarf.getEyeLocation();
 		center.add(center.getDirection());
 		
 		World world = center.getWorld();
-		world.spawnParticle(Particle.DRAGON_BREATH, center, (int) (souls + 50), 0.5, 0.1, 0.5, 0.05 + 0.005*souls);
-		world.spawnParticle(Particle.SMOKE_NORMAL, center, (int) (5*souls + 100), 1.5, 1,  1.5, 0.003);
+		world.spawnParticle(Particle.DRAGON_BREATH, center, (int) soulScaling(50, 250), 0.5, 0.1, 0.5, 0.05 + 0.005*souls);
+		world.spawnParticle(Particle.SMOKE_NORMAL, center, (int) soulScaling(100, 500), 1.5, 1,  1.5, 0.003);
 		
-		float pitch = 0.5f + (float) (0.008f * souls);
+		float pitch = (float) soulScaling(0.5, 0.9);
 		world.playSound(center,"dash", 1f, pitch);
 		world.playSound(center,"entity.generic.burn", 1f, pitch);
 		
-		double kb = 1 + 0.04 * souls;
-		double baseDamage = souls * 4;
+		double kb = soulScaling(1, 3);
+		double baseDamage = soulScaling(25, 150);
 		
 		souls = 0;
 		
@@ -111,14 +144,30 @@ public class Soulblade extends AbstractItem implements CooldownPiece {
 		}
 	}
 	
-	private void soulCheck() {
-		if (souls > MAX_SOULS) {
-			souls = MAX_SOULS;
-		}
-	}
 	
-	@Override
-	public float getCooldown() {
-		return Math.min(1, (float) souls/MAX_SOULS);
+	private static final double TWOPI = 2*Math.PI;
+	
+	private double polar = 0;
+	private double azimuthal = 0;
+	
+	private void showParticle() {
+		double velocity = soulScaling(0.1, 0.2);
+		polar     = (polar     + velocity) % TWOPI;
+		azimuthal = (azimuthal + velocity*velocity) % TWOPI;
+		
+		
+		int numParticles = (int) soulScaling(0, 15);
+		Location center = dwarf.getEyeLocation().subtract(0, 0.5, 0);
+		for (int i=0; i<numParticles; i++) {
+			double particlePolar = polar     + i;
+			double particleAzi   = azimuthal + i;
+			particlePolar = particlePolar % TWOPI;
+			particleAzi   = particleAzi   % TWOPI;
+			
+			//particlePolar = (particlePolar <= Math.PI ? particlePolar : TWOPI - particlePolar);
+			
+			Location particleLocation = Util.getSphericalPosition(center, 1.5, particlePolar, particleAzi);
+			particleLocation.getWorld().spawnParticle(Particle.REDSTONE, particleLocation, 0, 0.75, 0.1, 0.8);
+		}
 	}
 }
--- Game/src/main/java/deimophobe/nightfall/util/Util.java
+++ Game/src/main/java/deimophobe/nightfall/util/Util.java
@@ -1,5 +1,6 @@
 package deimophobe.nightfall.util;
 
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 
 /**
@@ -15,6 +16,20 @@ public class Util {
 				
 			default:
 				return false;
+		}
+	}
+	
+	public static Location getSphericalPosition(Location center, double radius, double polar, double azimuthal) {
+		double x = radius * Math.sin(polar) * Math.cos(azimuthal);
+		double z = radius * Math.sin(polar) * Math.sin(azimuthal);
+		double y = radius * Math.cos(polar);
+		
+		return center.clone().add(x,y,z);
+	}
+	
+	public static void doNTimes(int n, Runnable runnable) {
+		for (int i=0; i<n; i++) {
+			runnable.run();
 		}
 	}
 }
