dc25ccfdd41b3a913449906570c7c689d599ba07
Deimophobe <deimophobe@gmail.com>
2018-05-08 23:09:59 +1200

    Wrenches equip armour on use

--- Game/src/main/java/deimophobe/nightfall/dwarf/consumable/Wrench.java
+++ Game/src/main/java/deimophobe/nightfall/dwarf/consumable/Wrench.java
@@ -2,6 +2,8 @@ package deimophobe.nightfall.dwarf.consumable;
 
 import deimophobe.nightfall.ClickType;
 import deimophobe.nightfall.dwarf.Dwarf;
+import deimophobe.nightfall.dwarf.armour.Armour;
+import deimophobe.nightfall.dwarf.armour.DwarvenArmour;
 import deimophobe.nightfall.map.GameMap;
 import org.bukkit.ChatColor;
 import org.bukkit.block.Block;
@@ -20,14 +22,24 @@ class Wrench extends Consumable {
 	public int use(Dwarf dwarf, ClickType click, Block clickedBlock, BlockFace face) {
 		if (click.isRightClick()) return FAILED_CD;
 		if (!checkPhase(dwarf)) return FAILED_CD;
-		if (!dwarf.getArmour().canPickRepair()) {
+		
+		boolean canUse = false;
+		Armour armour = dwarf.getArmour();
+		if (armour.canPickRepair()) canUse = true;
+		if (!armour.isArmoured()) canUse = true;
+	
+		if (canUse) {
+			boolean success = GameMap.getCurrentMap().useGold(75);
+			armour.repair(success ? 1000 : 250);
+			dwarf.playSound("block.anvil.use", 20, 0.8f, false);
+			
+			if (!armour.isArmoured() && armour instanceof DwarvenArmour) {
+				((DwarvenArmour) armour).putOn();
+			}
+			return DEFAULT_CD;
+		} else {
 			dwarf.sendTitleMessage(ChatColor.GOLD + "Your armour is nearly full!");
 			return FAILED_CD;
 		}
-		
-		boolean success = GameMap.getCurrentMap().useGold(75);
-		dwarf.getArmour().repair( success ? 1000 : 250 );
-		dwarf.playSound("block.anvil.use", 20, 0.8f, false);
-		return DEFAULT_CD;
 	}
 }
