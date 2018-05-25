3c20557e74c285eff2e1ecdbe885e047ebc7ce2f
Deimophobe <deimophobe@gmail.com>
2018-05-25 21:12:28 +1200

    Horn only works on left click

--- Game/src/main/java/deimophobe/nightfall/dwarf/kit/hero/Horn.java
+++ Game/src/main/java/deimophobe/nightfall/dwarf/kit/hero/Horn.java
@@ -3,12 +3,14 @@ package deimophobe.nightfall.dwarf.kit.hero;
 import deimophobe.nightfall.ClickType;
 import deimophobe.nightfall.NightfallPlugin;
 import deimophobe.nightfall.common.items.CustomItem;
+import deimophobe.nightfall.cooldown.Cooldown;
+import deimophobe.nightfall.cooldown.UseCooldown;
 import deimophobe.nightfall.dwarf.Dwarf;
 import deimophobe.nightfall.dwarf.DwarfManager;
 import deimophobe.nightfall.dwarf.DwarvenItems;
 import deimophobe.nightfall.dwarf.ProcType;
+import deimophobe.nightfall.dwarf.kit.AbstractItem;
 import deimophobe.nightfall.dwarf.kit.KitGiveType;
-import deimophobe.nightfall.dwarf.kit.AbstractCooldownItem;
 import deimophobe.nightfall.effects.sound.Sounds;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -17,24 +19,25 @@ import org.bukkit.scheduler.BukkitRunnable;
 /**
  * Created by Deimophobe on 13/05/17.
  */
-public class Horn extends AbstractCooldownItem {
+public class Horn extends AbstractItem {
 	
 	public Horn(Dwarf dwarf) {
-		super(dwarf, 120*20);
+		super(dwarf);
 	}
 	
 	private final static CustomItem ITEM = DwarvenItems.getItem("hero", "horn");
 	@Override public CustomItem getItem() {return ITEM;}
 	@Override public KitGiveType getGiveType() {return KitGiveType.START;}
 	
+	private final Cooldown cooldown = new UseCooldown(120*20, Horn::tootHorn);
+	
 	@Override
 	public boolean onUse(ClickType click, Block block, BlockFace face) {
-		if (isOffCD()) {
-			resetCooldown();
-			tootHorn();
-			return true;
+		if (click.isLeftClick()) {
+			return cooldown.tryUse();
+		} else {
+			return false;
 		}
-		return false;
 	}
 	
 	public static void tootHorn() {
