6aea8113bdd04bafc98cd33c9b963a04de6ad76d
Deimophobe <deimophobe@gmail.com>
2018-06-25 23:39:13 +1200

    Cooldown holder has 'queued updateables' to prevent concurrent modifications.

--- Game/src/main/java/deimophobe/nightfall/cooldown/CooldownHolder.java
+++ Game/src/main/java/deimophobe/nightfall/cooldown/CooldownHolder.java
@@ -13,12 +13,24 @@ public class CooldownHolder implements Updateable {
 	private final Set<Updateable> updateables = new HashSet<>();
 	private final Set<Expirable> expirables = new HashSet<>();
 	
+	// Only used to temporary hold any new items to prevent concurrent modifications
+	// from updateables creating more updateables (note this is not a multithread issue).
+	private final Set<Updateable> queuedUpdateables = new HashSet<>();
+	private boolean updating;
+	
 	@Override
 	public void update() {
+		updating = true;
+		
 		updateables.forEach(Updateable::update);
 		removeExpireables();
 		expirables.forEach(Expirable::update);
 		removeExpireables();
+		
+		updating = false;
+		
+		queuedUpdateables.forEach(this::addUpdateable);
+		queuedUpdateables.clear();
 	}
 	
 	private void removeExpireables() {
@@ -34,11 +46,14 @@ public class CooldownHolder implements Updateable {
 	
 	public void addUpdateable(Updateable updateable) {
 		checkNotNull(updateable, "Updateable must not be null");
-		
-		if (updateable instanceof Expirable) {
-			expirables.add((Expirable) updateable);
+		if (updating) {
+			queuedUpdateables.add(updateable);
 		} else {
-			updateables.add(updateable);
+			if (updateable instanceof Expirable) {
+				expirables.add((Expirable) updateable);
+			} else {
+				updateables.add(updateable);
+			}
 		}
 	}
 }
