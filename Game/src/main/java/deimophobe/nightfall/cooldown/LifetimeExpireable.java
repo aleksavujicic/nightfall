25114914124920d755c20fd21aede8d78d1129e6
Deimophobe <deimophobe@gmail.com>
2018-06-25 23:39:26 +1200

    Expanded LifetimeExpireable API

--- Game/src/main/java/deimophobe/nightfall/cooldown/LifetimeExpireable.java
+++ Game/src/main/java/deimophobe/nightfall/cooldown/LifetimeExpireable.java
@@ -6,13 +6,11 @@ import deimophobe.nightfall.NightfallPlugin;
  * Created by Deimophobe on 31/03/18.
  */
 public abstract class LifetimeExpireable implements Expirable {
+	private final int maxLifetime;
 	private int lifetime;
 	
-	public int getLifetime() {
-		return lifetime;
-	}
-	
 	protected LifetimeExpireable(int lifetime) {
+		this.maxLifetime = lifetime;
 		this.lifetime = lifetime;
 	}
 	
@@ -30,15 +28,24 @@ public abstract class LifetimeExpireable implements Expirable {
 	@Override
 	public void onExpiry() {}
 	
-	public boolean everyNTicks(int n) {
+	public final int getLifetime() {
+		return lifetime;
+	}
+	public final int getMaxLifetime() {
+		return maxLifetime;
+	}
+	
+	protected final boolean afterAtLeastNTicks(int n) { return lifetime <= maxLifetime - n; }
+	protected final boolean afterExactlyNTicks(int n) { return lifetime == maxLifetime - n; }
+	protected final boolean everyNTicks(int n) {
 		return lifetime % n == 0;
 	}
 	
-	public void expire() {
+	protected final void expire() {
 		lifetime = 0;
 	}
 	
-	public void reduceLifetime(int amount) {
+	protected final void reduceLifetime(int amount) {
 		if (!checkPositive()) return;
 		
 		lifetime = Math.max(lifetime - amount, 0);
