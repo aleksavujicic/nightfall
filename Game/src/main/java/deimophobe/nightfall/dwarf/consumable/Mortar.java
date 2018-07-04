cff1a48262b4c4275e38caf6312188b8c818ba14
Deimophobe <deimophobe@gmail.com>
2018-07-05 02:05:01 +1200

    Wizard mortar not useable during build phase

--- Game/src/main/java/deimophobe/nightfall/dwarf/consumable/Mortar.java
+++ Game/src/main/java/deimophobe/nightfall/dwarf/consumable/Mortar.java
@@ -17,6 +17,7 @@ class Mortar extends Consumable {
 	private final double blueChance;
 	private final Supplier<Float> pitch;
 	private final ConsumeResult success;
+	private final boolean useableBuildPhase;
 	
 	Mortar(String item, boolean wizzy) {
 		super(item);
@@ -25,11 +26,13 @@ class Mortar extends Consumable {
 			this.blueChance = 1;
 			this.pitch = () -> 0.5f;
 			this.success = ConsumeResult.successfulWithDuration(20);
+			this.useableBuildPhase = false;
 		} else {
 			this.range = 4;
 			this.blueChance = 0.02;
 			this.pitch = () -> (float) (0.5 + 0.05 * Math.random());
 			this.success = ConsumeResult.successfulWithDuration(60);
+			this.useableBuildPhase = true;
 		}
 	}
 	
@@ -37,6 +40,9 @@ class Mortar extends Consumable {
 	@Override
 	public ConsumeResult use(Dwarf dwarf, ClickType click, Block clickedBlock, BlockFace face) {
 		if (click.isRightClick()) return ConsumeResult.FAILURE;
+		ConsumeResult phaseCheck = checkPhase();
+		if (!useableBuildPhase && phaseCheck != null) return phaseCheck;
+		
 		
 		double chance = (Game.getGame().getPhase().haveMonstersBeenReleased() ? blueChance : 1);
 		BlockConverter.mortar(clickedBlock, range, chance);
