ecdffcae656228455f9837d3297fb4859fdd3a8f
Deimophobe <deimophobe@gmail.com>
2018-09-25 22:12:02 +1200

    Number tweaking

--- Game/src/main/java/deimophobe/nightfall/monster/mob/GoblinKaboom.java
+++ Game/src/main/java/deimophobe/nightfall/monster/mob/GoblinKaboom.java
@@ -137,7 +137,7 @@ class GoblinKaboom extends Goblin {
             Vector knockback = offset.normalize().multiply(kb * (1 - offlength / range));
 			knockback.setY(knockback.getY() / 2 + 0.1 + (superKaboom ? 0.4 : 0));
 			
-			double damageMult = Math.min(0.4*range/offlength, 1);
+			double damageMult = Math.min(0.3*range/offlength, 1);
 			dwarfDamage *= damageMult;
 			
 			DwarfDamage aoeDamage = dwarf.createDamage(monster, GameDamageType.GOBO_KABOOM, dwarfDamage);
