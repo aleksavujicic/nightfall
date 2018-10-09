127a57e664134b966dc90d87e3dade98420118ce
Deimophobe <deimophobe@gmail.com>
2018-10-09 18:21:10 +1300

    Sabs no longer unhide when r clicking on vines

--- Game/src/main/java/deimophobe/nightfall/monster/mob/ZombieSaboteur.java
+++ Game/src/main/java/deimophobe/nightfall/monster/mob/ZombieSaboteur.java
@@ -127,7 +127,6 @@ public class ZombieSaboteur extends ZombieMob {
 		
 		if (click.isRightClick() && isPlayerHoldingItem("vines")) {
 			placeVine(block, face);
-			unhide();
 		}
 		
 		if (isPlayerHoldingItem("unhide")) {
