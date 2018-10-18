f54c93504446b1ccbdc6df08bc392f96946be5e3
Deimophobe <deimophobe@gmail.com>
2018-10-18 21:24:30 +1300

    Bloodied waraxe only heals on its own proc

--- Game/src/main/java/deimophobe/nightfall/dwarf/kit/melee/BloodiedWaraxe.java
+++ Game/src/main/java/deimophobe/nightfall/dwarf/kit/melee/BloodiedWaraxe.java
@@ -41,7 +41,7 @@ public class BloodiedWaraxe extends AbstractItem implements CooldownPiece {
 	@Override
 	public void onKill(MonsterDamage damage) {
 		cd.reduceCooldown(20);
-		if (dwarf.hasProc() && isMeleeDamageFromItem(damage)) {
+		if (dwarf.hasProc(ProcType.MALICE) && isMeleeDamageFromItem(damage)) {
 			dwarf.heal(5);
 			dwarf.regenMana(5);
 		}
