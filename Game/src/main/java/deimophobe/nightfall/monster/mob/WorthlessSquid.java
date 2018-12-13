e6a01be04e06a0525881dc641d9487408dc4b529
Deimophobe <deimophobe@gmail.com>
2018-12-13 23:50:57 +1300

    Kinda fixed squid disguise flying off into space

--- Game/src/main/java/deimophobe/nightfall/monster/mob/WorthlessSquid.java
+++ Game/src/main/java/deimophobe/nightfall/monster/mob/WorthlessSquid.java
@@ -1,5 +1,11 @@
 package deimophobe.nightfall.monster.mob;
 
+import com.comphenix.packetwrapper.WrapperPlayServerEntityTeleport;
+import com.comphenix.packetwrapper.WrapperPlayServerEntityVelocity;
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
 import deimophobe.nightfall.ClickType;
 import deimophobe.nightfall.cooldown.*;
 import deimophobe.nightfall.map.GameMap;
@@ -7,12 +13,16 @@ import deimophobe.nightfall.monster.MonsterPlayer;
 import deimophobe.nightfall.monster.SpawnMethod;
 import deimophobe.nightfall.util.PacketUtil;
 import deimophobe.nightfall.util.Util;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
 import org.bukkit.material.MaterialData;
+
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Created by Deimophobe on 11/10/18.
@@ -25,7 +35,7 @@ final class WorthlessSquid extends AbstractMob {
 	}
 	
 	@Update @Display
-	@Interact(click = ClickType.RIGHT, item = "test")
+	@Interact(click = ClickType.RIGHT)
 	private final Cooldown squirtCD = new UseCooldown(200, this::squirt);
 	
 	@Override
@@ -53,5 +63,32 @@ final class WorthlessSquid extends AbstractMob {
 		monster.getWorld().spawnParticle(Particle.BLOCK_DUST, monster.getLocation(), 30, 0.5, 0.5, 0.5, 0.05, INK_BLOCK_DATA);
 		monster.getWorld().spawnParticle(Particle.BLOCK_CRACK, monster.getLocation(), 30, 0.5, 0.5, 0.5, 0.05, INK_BLOCK_DATA);
 		monster.getWorld().spawnParticle(Particle.SMOKE_LARGE, monster.getLocation(), 5, 0.5, 0.5, 0.5, 0.05);
+		
+		// To prevent the disguise from bugging out
+		monster.doLater(() -> {
+			Location location = monster.getLocation();
+			
+			WrapperPlayServerEntityTeleport packet = new WrapperPlayServerEntityTeleport();
+			packet.setEntityID(monster.getPlayer().getEntityId());
+			packet.setOnGround(monster.getPlayer().isOnGround());
+			packet.setX(location.getX());
+			packet.setY(location.getY());
+			packet.setZ(location.getZ());
+			packet.setYaw(location.getYaw());
+			packet.setPitch(location.getPitch());
+			
+			WrapperPlayServerEntityVelocity velocityPacket = new WrapperPlayServerEntityVelocity();
+			velocityPacket.setEntityID(monster.getPlayer().getEntityId());
+			velocityPacket.setVelocityX(monster.getVelocity().getX());
+			velocityPacket.setVelocityY(monster.getVelocity().getY());
+			velocityPacket.setVelocityZ(monster.getVelocity().getZ());
+			
+			for (Player player : Bukkit.getOnlinePlayers()) {
+				if (player != monster.getPlayer()) {
+					packet.sendPacket(player);
+					velocityPacket.sendPacket(player);
+				}
+			}
+		}, 10);
 	}
 }
