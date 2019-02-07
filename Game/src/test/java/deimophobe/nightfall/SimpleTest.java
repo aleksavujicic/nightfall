8cc2141013287c1ecb76dac944254a5689453804
Deimophobe <deimophobe@gmail.com>
2019-02-07 22:24:58 +1300

    Added some config for automatic installing of jar files, and also basic unit testing

---
+++ Game/src/test/java/deimophobe/nightfall/SimpleTest.java
@@ -0,0 +1,17 @@
+package deimophobe.nightfall;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNull;
+
+
+/**
+ * Created by Deimophobe on 7/02/19.
+ */
+public class SimpleTest {
+	
+	@Test
+	public void testWorks() {
+		assertNull(null);
+	}
+}
